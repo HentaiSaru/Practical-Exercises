fd786f3e9a73a0e3c1cc91d2a445d8d0e3ab8d4b
HentaiSaru <s3459897@gmail.com>
2023-05-06 17:53:04 +0800

    早期練習23

---
+++ Python/早期練習題/練習23.py
@@ -0,0 +1,32 @@
+""" 題目說明
+
+請撰寫一程式，提示使用者輸入一個社會安全碼SSN，
+格式為ddd-dd-dddd，d表示數字。
+若格式完全符合（正確的SSN）則顯示【Valid SSN】，
+否則顯示【Invalid SSN】。
+
+輸入說明
+一個字串（格式為ddd-dd-dddd，d表示數字）
+
+輸出說明
+判斷是否符合SSN格式
+
+範例輸入1
+329-48-4977
+
+範例輸出1
+Valid SSN
+
+範例輸入2
+837-a3-3000
+
+範例輸出2
+Invalid SSN
+
+"""
+n = input("輸入SSN碼:").replace("-", "")
+
+if n.isdigit():
+    print("Valid SSN")
+else:
+    print("Invalid SSN")
