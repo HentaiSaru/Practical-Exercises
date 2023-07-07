71a56673873f13c56630e0a9d31769c54353e0ba
TenshinoOtoKafu <s3459897@gmail.com>
2023-07-08 00:34:02 +0800

    測試更新

--- JavaScript/E-HentaiAutoLogin.js
+++ JavaScript/E-HentaiAutoLogin.js
@@ -2,7 +2,7 @@
 // @name         (E/Ex-Hentai) AutoLogin
 // @namespace    http://tampermonkey.net/
 
-// @version      0.0.3
+// @version      0.0.4
 // @author       HentiSaru
 // @description  檢測 E 站的登入狀態 , 沒有登入就添加 cookie 進去
 
@@ -36,28 +36,56 @@
 // [RequiredCookies] 需要存在的 cookie / [LoginCookies] 登入 cookie (字典)
 var RequiredCookies = ["ipb_member_id","ipb_pass_hash"] , LoginCookies;
 GM_registerMenuCommand("登入 Cookie 設置", CookieSettings);// 設置選單(顯示)
+var domain = window.location.hostname; // 取得域名
+var custom = false; // 自訂使用狀態
 
-// localStorage.getItem() 將保存在本地 , 直到數據被清除 , 不然理論上永久;
+// localStorage.getItem() 將保存在本地 , 直到數據被清除 , 不然理論上永久 , 受同源政策限制;
 var UseCheck = sessionStorage.getItem('UseCheck');// 判斷是否使用檢查方法(會話檢測)
 var NoReminderSet = sessionStorage.getItem('NoReminderSet');// 判斷是否提醒過 設置cookie
 /* ==================== 全局設置變數 ==================== */
 
-/* ==================== 檢查會話狀態/呼叫檢查方法 ==================== */
-if (!UseCheck) {
-    // 從 localStorage 中獲取存儲的 Cookies 字串 , 轉換為 LoginCookies 對象
-    var cookies = localStorage.getItem("E/Ex_Cookies")
+/* ==================== 檢查會話狀態/呼叫檢查方法 [運行入口點] ==================== */
+// 使用自訂方法 , 就不會受到同源政策影響 , 但需要修改代碼 , 並將 cookie 輸入至 , CustomCookies() 方法內
+// CustomCookies()
 
-    if (cookies !== null) {
-        LoginCookies = JSON.parse(cookies);
-        // 啟用檢測
-        CheckCookies();
-    } else {
-        if (!NoReminderSet) {
-            alert("未檢測到設置的 Cookies\n請從 [登入 Cookie 設置] 選單中進行設置");
-            sessionStorage.setItem('NoReminderSet', true);
+if (!UseCheck) {
+    if (domain === "exhentai.org") {
+        RequiredCookies.push("igneous");
+    }
+    if (!custom) { // 檢查是否使用自訂
+        // 從 localStorage 中獲取存儲的 Cookies 字串 , 轉換為 LoginCookies 對象
+        var cookies = localStorage.getItem("E/Ex_Cookies")
+        if (cookies !== null) {
+            LoginCookies = JSON.parse(cookies);
+            // 啟用檢測
+            CheckCookies();
+        } else {
+            if (!NoReminderSet) {
+                if (domain === "e-hentai.org") {
+                    alert("根據同源政策表裏站必須分別設置\ne-hentai未設置 Cookies\n請從 [登入 Cookie 設置] 選單中進行設置");
+                } else if (domain === "exhentai.org") {
+                    alert("根據同源政策表裏站必須分別設置\nexhentai未設置 Cookies\n請從 [登入 Cookie 設置] 選單中進行設置");
+                }
+                sessionStorage.setItem('NoReminderSet', true);
+            }
         }
+    } else {
+        CheckCookies();
     }
 }/* ==================== 檢查會話狀態/呼叫檢查方法 ==================== */
+
+/* ==================== 自訂 Cookies ==================== */
+function CustomCookies() {
+    LoginCookies = [
+        { name: "igneous", value: "" },
+        { name: "ipb_member_id", value: "" },
+        { name: "ipb_pass_hash", value: "" },
+        { name: "sk", value: "gy8wgij076agx1ax6is9htzrj40i" },
+        { name: "yay", value: "louder" },
+        { name: "sl", value: "dm_2" }
+    ];
+    custom = true;
+}/* ==================== 自訂 Cookies ==================== */
 
 /* ==================== 檢查 cookie 狀態 ==================== */
 function CheckCookies() {
@@ -65,7 +93,7 @@ function CheckCookies() {
     var cookiesFound = RequiredCookies.every(function(cookieName) {
         return cookies.hasOwnProperty(cookieName);
     });
-
+    
     // 如果未找到所有指定的 cookie，則進行相應操作
     if (!cookiesFound) {
         // 刪除當前頁面的所有 cookie
@@ -77,9 +105,9 @@ function CheckCookies() {
     }
 
     // 判斷 exhentai 的 cookie 值是否符合要求
-    var currentDomain = window.location.hostname;
-    if (currentDomain === "exhentai.org" && cookies.igneous === "mystery") {
+    if (domain === "exhentai.org" & cookies.igneous === "mystery") {
         DeleteAllCookies();
+        AddCookies();
         location.reload();
     }
 
@@ -122,17 +150,21 @@ function DeleteAllCookies() {
 /* ==================== cookie 設置函式 ==================== */
 function CookieSettings() {
     // 設定的項目
-    var igneous , ipb_member_id , ipb_pass_hash , Input;
+    var igneous=null , ipb_member_id , ipb_pass_hash , Input;
 
-    // 使用 prompt 輸入框進行添加數據
-    Input = prompt("[可直接取消]\n輸入 igneous：");
-    if (Input !== null) {
-        igneous = Input.trim();
-    } else {
-        igneous = null
+    if (domain === "exhentai.org") {
+        while (true) {
+            Input = prompt("輸入 igneous：");
+            if (Input !== null) {
+                igneous = Input.trim();
+                break;
+            } else {
+                alert("[igneous] 不得為空");
+            }
+        }
     }
     while (true) {
-        Input = prompt("[*必要輸入]\n輸入 ipb_member_id：");
+        Input = prompt("輸入 ipb_member_id：");
         if (Input !== null) {
             ipb_member_id = Input.trim();
             break;
@@ -141,7 +173,7 @@ function CookieSettings() {
         }
     }
     while (true) {
-        Input = prompt('[*必要輸入]\n輸入 ipb_pass_hash：');
+        Input = prompt("輸入 ipb_pass_hash：");
         if (Input !== null) {
             ipb_pass_hash = Input.trim();
             break;
@@ -181,8 +213,7 @@ function CookieSettings() {
     // 確認正確，進行保存
     if (confirmed) {
         // 將 LoginCookies 對象轉換為字符串並存儲在 localStorage 中
-        var CookiesJson = JSON.stringify(LoginCookies);
-        localStorage.setItem("E/Ex_Cookies", CookiesJson);
+        localStorage.setItem("E/Ex_Cookies", JSON.stringify(LoginCookies));
         alert("保存成功");
         location.reload();
     } else {
