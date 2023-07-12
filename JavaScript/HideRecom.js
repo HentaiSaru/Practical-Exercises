5089e265eb91faf337f0544cb2d6faa3beeb8032
TenshinoOtoKafu <s3459897@gmail.com>
2023-07-13 01:07:56 +0800

    聲明修正

--- JavaScript/HideRecom.js
+++ JavaScript/HideRecom.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @version      0.0.1
+// @version      0.0.2
 // @author       HentaiSaru
 // @name         YouTube 隱藏播放建議
 // @description  將影片結尾推薦框透明化 , 滑鼠懸浮恢復 , 按下 Shift 則完全隱藏 , 再次按下恢復
 // @icon         https://cdn-icons-png.flaticon.com/512/1383/1383260.png
 
 // @license      MIT
-// @homepageURL  https://github.com/hoothin/
-// @supportURL   https://github.com/hoothin/
-
 // @grant        GM_addStyle
 // @run-at       document-start
 // @match        *://www.youtube.com/*
-
-// Original      code by hoothin under MIT License
-// Original      Code Link : [https://greasyfork.org/zh-TW/scripts/438403-youtube-hide-related-suggestion-which-occlude-the-video]
 // ==/UserScript==
+
+/* 
+Original Author Page : [https://github.com/hoothin/]
+Original Author Link : [https://greasyfork.org/zh-TW/scripts/438403-youtube-hide-related-suggestion-which-occlude-the-video]
+*/
+
 (function() {
     let css = `
         .ytp-ce-element{opacity: 0.1!important;}
@@ -28,7 +28,6 @@
         ElementNode.appendChild(document.createTextNode(css));
         (document.querySelector("head") || document.documentElement).appendChild(ElementNode);
     }
-  
     document.addEventListener("keydown", function(event) {
         if (event.shiftKey) {
             event.preventDefault();
