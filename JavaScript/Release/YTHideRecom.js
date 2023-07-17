e6ca9a1452742913c392db693cbe81abc2188aab
TenshinoOtoKafu <s3459897@gmail.com>
2023-07-17 08:29:42 +0800

    新增隱藏推薦影片功能

--- JavaScript/Release/YTHideRecom.js
+++ JavaScript/Release/YTHideRecom.js
@@ -1,22 +1,33 @@
 // ==UserScript==
-// @version      0.0.2
+// @version      0.0.3
 // @author       HentaiSaru
-// @name         YT Hide Recom
+// @name         YT Hide Recom Tool
 // @description  將影片結尾推薦框透明化 , 滑鼠懸浮恢復 , 按下 Shift 則完全隱藏 , 再次按下恢復
 // @icon         https://cdn-icons-png.flaticon.com/512/1383/1383260.png
 
-// @license      MIT
-// @grant        GM_addStyle
 // @run-at       document-start
 // @match        *://www.youtube.com/*
+
+// @license      MIT
+// @grant        GM_addStyle
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 /*
 Original Author Page : [https://github.com/hoothin/]
 Original Author Link : [https://greasyfork.org/zh-TW/scripts/438403-youtube-hide-related-suggestion-which-occlude-the-video]
+
+備註 :
+document.getElementById('below') / 包含影片訊息到留言元素
 */
 
 (function() {
+    const Mene = GM_registerMenuCommand(
+        "📜 [功能說明]",
+        function() {
+            alert("Shift : 完全隱藏影片尾部推薦\nCtrl : 隱藏右側影片推薦");
+        }
+    )
     let css = `
         .ytp-ce-element{opacity: 0.1!important;}
         .ytp-ce-element:hover{opacity: 1!important;}
@@ -39,6 +50,14 @@ Original Author Link : [https://greasyfork.org/zh-TW/scripts/438403-youtube-hide
                     element.style.display = "none";
                 }
             });
+        } else if (event.ctrlKey) {
+            event.preventDefault();
+            let element = document.getElementById('secondary');
+            if (element.style.display === "none") {
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
         }
     });
 })();
