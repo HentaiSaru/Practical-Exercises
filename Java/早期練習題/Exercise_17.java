a0d2a6a4808b402427bd80fdb73a28b6d2320a2e
TenshinoOtoKafu <s3459897@gmail.com>
2023-07-25 23:56:29 +0800

    後續補充

---
+++ Java/早期練習題/Exercise_17.java
@@ -0,0 +1,73 @@
+/* 題目說明
+
+專案已內含名為Account類別，類別內含name、rate及balance變數，分別代表戶名、利率（單位%）及餘額。
+請在Account類別建立五個方法，分別是setRate、deposit、withdraw、getBalance、getFutureValue，功能分別是設定年利率、存款，提款、查詢餘額、餘額加計利息。
+請撰寫程式，讀取read.txt檔案，內含jack的五筆存提款記錄，第三個欄位的d代表存款、w代表提款，以迴圈逐筆執行存提款動作。
+使用者輸入一個1~5的正整數，代表年利率（單位%），依照目前jack的本金進行本利和計算，輸出一年後的本利和總計；若輸入不在指定範圍的資料，請輸出【error】。
+本利和（年）計算公式：本金×(1 + 年利率)。
+
+輸入說明
+年利率
+
+輸出說明
+一年後本金及利息總計（輸出最後一行後不自動換行）
+________________________________________
+範例輸入1
+1
+
+範例輸出1
+555.5
+
+範例輸入2
+4
+
+範例輸出2
+572
+
+範例輸入3
+6
+
+範例輸出3
+error
+
+*/
+
+import java.util.*;
+
+class Account {
+    String name;
+    double rate;
+    double balance;
+
+    // 設定年利率
+    void setRate() {
+
+    }
+
+    // 存款
+    void deposit() {
+
+    }
+
+    // 提款
+    void withdraw() {
+
+    }
+
+    // 查詢餘額
+    void getBalance() {
+
+    }
+
+    // 餘額加計利息
+    void getFutureValue() {
+
+    }
+
+}
+
+public class Exercise_17 {
+    public static void main(String[] args){
+
+    }
+}
