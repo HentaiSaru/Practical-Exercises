db35cb97f6dda3dc36f600af2a2eb18bd879fb3c
TenshinoOtoKafu <s3459897@gmail.com>
2023-08-01 21:41:50 +0800

    測試待開發

---
+++ JavaScript/Beta/yt.js
@@ -0,0 +1,16 @@
+/*width: 1429px;
+height: 754px;
+left: 0px;
+top: -754px;*/
+
+let Video = document.querySelector("video");
+Video.style.left = "0rem";
+Video.style.top = "0rem";
+Video.style.width = "1920px";
+Video.style.height = "1080px";
+
+// 預設模式這個地方不會有東西 (劇院模式會放到這邊來)
+//<div id="player-container-inner" class="style-scope ytd-watch-flexy"></div>
+
+// 預設模式的內容再這邊
+//<div id="player-wide-container" class="style-scope ytd-watch-flexy"></div>
