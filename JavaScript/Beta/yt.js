ef795fdd65166e75819d6a1a13e62d7d06bb974b
TenshinoOtoKafu <s3459897@gmail.com>
2023-08-02 02:37:54 +0800

    測試

--- JavaScript/Beta/yt.js
+++ JavaScript/Beta/yt.js
@@ -14,3 +14,13 @@ Video.style.height = "1080px";
 
 // 預設模式的內容再這邊
 //<div id="player-wide-container" class="style-scope ytd-watch-flexy"></div>
+
+// 監聽網頁上的點選事件
+document.addEventListener("click", function(event) {
+    // 確認點選的是連結
+    if (event.target.tagName === "A") {
+      event.preventDefault(); // 阻斷原本的跳轉
+      const url = event.target.href; // 獲取連結的網址
+      GM_openInTab(url, { active: true, insert: true }); // 在新分頁中開啟連結
+    }
+  });
