f5731321a516559a91401fcfe17b1288fe83779b
TenshinoOtoKafu <s3459897@gmail.com>
2023-08-03 23:23:29 +0800

    排版調整

--- JavaScript/Release/KemonoDownloadTool.js
+++ JavaScript/Release/KemonoDownloadTool.js
@@ -4,6 +4,8 @@
 // @name:zh-CN   Kemono 下载工具
 // @name:ja      Kemono ダウンロードツール
 // @name:en      Kemono DownloadTool
+// @version      0.0.4
+// @author       HentiSaru
 // @description:        一鍵下載圖片 (壓縮下載/單圖下載) , 頁面數據創建 json 下載 , 一鍵開啟當前所有帖子
 // @description:zh-TW   一鍵下載圖片 (壓縮下載/單圖下載) , 頁面數據創建 json 下載 , 一鍵開啟當前所有帖子
 // @description:zh-CN   一键下载图片 (压缩下载/单图下载) , 页面数据创建 json 下载 , 一键开启当前所有帖子
@@ -17,8 +19,6 @@
 // @icon         https://cdn-icons-png.flaticon.com/512/2381/2381981.png
 
 // @license      MIT
-// @author       HentiSaru
-// @version      0.0.4
 // @namespace    https://greasyfork.org/users/989635
 
 // @run-at       document-end
