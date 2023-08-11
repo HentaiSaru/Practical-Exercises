21c947a53e99c5c0a44ffc53a9fff184cf30994d
TenshinoOtoKafu <s3459897@gmail.com>
2023-08-11 16:52:01 +0800

    代碼調整

--- JavaScript/Release/WnacgOptimization.js
+++ JavaScript/Release/WnacgOptimization.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            wnacg 漫畫一頁化
-// @version         0.0.2
+// @version         0.0.3
 // @author          HentiSaru
 // @description     只設置匹配 https://www.wnacg.com/  漫畫自動載入所有頁面至同一頁, 純黑背景色, 圖像最大寬度縮小
 
@@ -11,24 +11,11 @@
 // @namespace       https://greasyfork.org/users/989635
 
 // @run-at          document-start
+// @grant           GM_addStyle
 
 // @require         https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js
 // @require         https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js
 // ==/UserScript==
-
-(function() {
-    let photo_box, current, total, interval = setInterval(() => {
-        photo_box = document.getElementById("photo_body");
-        current = document.querySelector("span.newpagelabel b");
-        total = document.querySelectorAll("select option");
-        if (photo_box && current && total.length > 0) {
-            document.body.style.cssText = "overflow-x: visible !important; background: #000000;";
-            ImageGeneration(photo_box, current, total);
-            AdReplace();
-            clearInterval(interval);
-        }
-    }, 500);
-})();
 
 var observer = new IntersectionObserver(function(observed) {
     observed.forEach(function(entry) {
@@ -38,10 +25,41 @@ var observer = new IntersectionObserver(function(observed) {
     });
 }, {threshold: 0.5});
 
+(function() {
+    GM_addStyle(`
+        .ImageOptimization {
+            display: block;
+            margin: 0 auto;
+            width: 100%;
+            max-width: 60%;
+        }
+        .ImageBorder {
+            border-top: 1rem solid white;
+            border-bottom: 1rem solid white;
+        }
+        .CustomBody {
+            overflow-x: visible !important;
+            background: #000000;
+        }
+    `);
+    let photo_box, current, total, interval = setInterval(() => {
+        photo_box = document.getElementById("photo_body");
+        current = document.querySelector("span.newpagelabel b");
+        total = document.querySelectorAll("select option");
+        if (photo_box && current && total.length > 0) {
+            document.body.classList.add("CustomBody");
+            photo_box.classList.remove("photo_body");
+            ImageGeneration(photo_box, current, total);
+            AdReplace();
+            clearInterval(interval);
+        }
+    }, 500);
+})();
+
+/* 渲染 */
 function ReactRender({link, src}) {
     return React.createElement("img", {
-        className: "photo",
-        style: { display: "block", margin: "0px auto", width: "100%", maxWidth: "60%" },
+        className: "ImageOptimization",
         src: src,
         alt: link,
         ref: function(img) {if (img) {observer.observe(img)}}
@@ -51,6 +69,7 @@ async function ImageGeneration(box, current, total) {
     // 總頁數 = 總共頁數 - 當前頁數
     var total_pages = total.length - parseInt(current.textContent, 10);
     let link, img, dom, parser = new DOMParser();
+    document.title = document.title.split(" - ")[1];
     link = box.querySelector("a").href;
     img = box.querySelector("img").src;
     ReactDOM.render(React.createElement(ReactRender, { link:link, src: img }), box);
@@ -75,6 +94,7 @@ async function ImageGeneration(box, current, total) {
     }
 }
 
+/* 廣告隱藏 */
 async function AdReplace() {
     const breadHTML = { __html: document.querySelector(".png.bread").innerHTML };
     ReactDOM.render(
@@ -84,6 +104,7 @@ async function AdReplace() {
     );
 }
 
+/* 刪除底部不需要物件 */
 async function TailDeletion() {
     // box.replaceChildren(); 刪除子節點方法
     try {
