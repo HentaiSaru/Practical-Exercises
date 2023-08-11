18401b47f563cba33ef53eab5897e4a91384ed11
TenshinoOtoKafu <s3459897@gmail.com>
2023-08-11 12:15:46 +0800

    測試項目

--- JavaScript/Release/ExDownload.js
+++ JavaScript/Release/ExDownload.js
@@ -5,14 +5,14 @@
 // @name:ja      [E/Ex-Hentai] ダウンローダー
 // @name:ko      [E/Ex-Hentai] 다운로더
 // @name:en      [E/Ex-Hentai] Downloader
-// @version      0.0.13
+// @version      0.0.1
 // @author       HentiSaru
-// @description         暫時無說明
-// @description:zh-TW   暫時無說明
-// @description:zh-CN   暫時無說明
-// @description:ja      暫時無說明
-// @description:ko      暫時無說明
-// @description:en      暫時無說明
+// @description         在 E 和 Ex 的漫畫頁面, 創建下載按鈕, 可使用[壓縮下載/單圖下載], 自動獲取圖片下載 
+// @description:zh-TW   在 E 和 Ex 的漫畫頁面, 創建下載按鈕, 可使用[壓縮下載/單圖下載], 自動獲取圖片下載
+// @description:zh-CN   在 E 和 Ex 的漫画页面, 创建下载按钮, 可使用[压缩下载/单图下载], 自动获取图片下载
+// @description:ja      EとExの漫画ページで、ダウンロードボタンを作成し、[圧縮ダウンロード/単一画像ダウンロード]を使用して、自動的に画像をダウンロードします。
+// @description:ko      E 및 Ex의 만화 페이지에서 다운로드 버튼을 만들고, [압축 다운로드/단일 이미지 다운로드]를 사용하여 이미지를 자동으로 다운로드합니다.
+// @description:en      On the comic pages of E and Ex, create a download button that can use [compressed download/single image download] to automatically download images.
 
 // @match        https://e-hentai.org/*
 // @match        https://exhentai.org/*
@@ -25,6 +25,7 @@
 // @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_download
 // @grant        GM_addElement
 // @grant        GM_xmlhttpRequest
 // @grant        GM_registerMenuCommand
@@ -33,10 +34,13 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
 // ==/UserScript==
 
-var parser = new DOMParser(),
+var count = 0,
+ModeDisplay,
+parser = new DOMParser(),
 OriginalTitle = document.title,
 url = window.location.href.split("?p=")[0],
-count = 0;
+CompressMode = GM_getValue("CompressedMode", []),
+language = display_language(navigator.language);
 
 (function() {
     const Ex_HManga = /https:\/\/exhentai\.org\/g\/\d+\/[a-zA-Z0-9]+/;
@@ -44,33 +48,52 @@ count = 0;
     if (Ex_HManga.test(url) || E_HManga.test(url)) {
         ButtonCreation();
     }
+    GM_registerMenuCommand(language[0], function() {DownloadModeSwitch()}, "C");
 })();
-
 
 /* 按鈕創建 */
 async function ButtonCreation() {
     GM_addStyle(`
         .Download_Button {
-            color: #5C0D12;
             float: right;
             width: 9rem;
             cursor: pointer;
             font-weight: bold;
-            padding: 1px 5px 2px;
             line-height: 20px;
-            border: 2px solid #9a7c7e;
             border-radius: 5px;
             position: relative;
-            background-color: #EDEADA;
+            padding: 1px 5px 2px;
             font-family: arial,helvetica,sans-serif;
         }
-        .Download_Button:hover {
-            color: rgb(143, 71, 1);
-            border: 2px dashed #B5A4A4;
+    `);
+    AdaptiveCSS(`
+        .Download_Button {
+            color: #5C0D12;
+            border: 2px solid #9a7c7e;
             background-color: #EDEADA;
+        }
+        .Download_Button:hover {
+            color: #8f4701;
+            border: 2px dashed #B5A4A4;
         }
         .Download_Button:disabled {
             color: #B5A4A4;
+            border: 2px dashed #B5A4A4;
+            cursor: default;
+        }
+        `,`
+        .Download_Button {
+            color: #b3b3b3;
+            border: 2px solid #34353b;
+            background-color: #2c2b2b;
+        }
+        .Download_Button:hover {
+            color: #f1f1f1;
+            border: 2px dashed #4f535b;
+        }
+        .Download_Button:disabled {
+            color: #4f535b;
+            border: 2px dashed #4f535b;
             cursor: default;
         }
     `);
@@ -79,9 +102,14 @@ async function ButtonCreation() {
         download_button = GM_addElement(document.querySelector("div#gd2"), "button", {
             class: "Download_Button"
         });
-        download_button.textContent = "壓縮下載";
+        if (CompressMode) {
+            ModeDisplay = language[1];
+        } else {
+            ModeDisplay = language[2];
+        }
+        download_button.textContent = ModeDisplay;
         download_button.addEventListener("click", function() {
-            download_button.textContent = "開始下載";
+            download_button.textContent = language[3];
             download_button.disabled = true;
             HomeDataProcessing(download_button);
         });
@@ -115,7 +143,7 @@ async function HomeDataProcessing(button) {
         if (title === "") {throw new Error()}
     } catch {
         title = document.getElementById("gn").textContent.trim();
-        if (title === "") {title = "未找到名稱"}
+        if (title === "") {title = language[4]}
     }
     title = IllegalFilter(title);
 
@@ -137,14 +165,13 @@ async function HomeDataProcessing(button) {
         const html = await response.text();
         GetLink(parser.parseFromString(html, "text/html"));
     }
-    
-    //GetLink(document);
-    const promises = [];
-    for (let i = 0; i < pages; i++) {
+
+    const promises = [FetchRequest(url)];
+    for (let i = 1; i < pages; i++) {
         promises.push(FetchRequest(`${url}?p=${i}`));
-        button.textContent = `獲取頁面: [${i+1}/${pages}]`;
+        button.textContent = `${language[5]}: [${i+1}/${pages}]`;
         count++;
-        if (count === 7) {
+        if (count === 5) {
             count = 0;
             await new Promise(resolve => setTimeout(resolve, 1000));
         }
@@ -170,18 +197,14 @@ async function ImageLinkProcessing(button, title, link) {
     async function FetchRequest(index, url) {
         const response = await fetch(url);
         const html = await response.text();
+        button.textContent = `${language[6]}: [${index}/${pages}]`;
         GetLink(index, parser.parseFromString(html, "text/html").querySelector("img#img"));
-        button.textContent = `獲取連結: [${index}/${pages}]`;
     }
 
     const promises = [];
     link.forEach(async (url, i) => {
         promises.push(FetchRequest(i, url));
-        count++;
-        if (count === 150) {
-            count = 0;
-            await new Promise(resolve => setTimeout(resolve, 1000));
-        }
+        await new Promise(resolve => setTimeout(resolve, 150));
     });
 
     await Promise.all(promises);
@@ -190,20 +213,19 @@ async function ImageLinkProcessing(button, title, link) {
 
 /* 下載觸發器 */
 async function DownloadTrigger(button, title, link) {
-    ZipDownload(button, title, link);
+    if (CompressMode) {ZipDownload(button, title, link)} 
+    else {ImageDownload(button, title, link)}
 }
 
 /* 壓縮下載 */
 async function ZipDownload(Button, Folder, ImgData) {
-    const zip = new JSZip(),
-    Total = ImgData.size;
-    let promises = [], progress = 1, link, mantissa, extension, BackgroundWork;
+    const zip = new JSZip(), Total = ImgData.size, promises = [];
+    let progress = 1, link, mantissa, extension, BackgroundWork, retry=0;
     async function Request(index) {
+        link = ImgData.get(index);
+        extension = GetExtension(link);
         return new Promise((resolve) => {
-            link = ImgData.get(index);
-            extension = GetExtension(link);
-            downloadImage();
-            function downloadImage() {
+            if (link !== undefined) {
                 GM_xmlhttpRequest({
                     method: "GET",
                     url: link,
@@ -214,24 +236,28 @@ async function ZipDownload(Button, Folder, ImgData) {
                             mantissa = (index + 1).toString().padStart(4, '0');
                             zip.file(`${Folder}/${mantissa}.${extension}`, response.response);
                             document.title = `[${progress}/${Total}]`;
-                            Button.textContent = `下載進度: [${progress}/${Total}]`;
+                            Button.textContent = `${language[7]}: [${progress}/${Total}]`;
                             progress++;
+                            resolve();
                         } else {
-                            downloadImage();
+                            retry++;
+                            if (retry < 10) {
+                                Request(index);
+                            }
                         }
-                        resolve();
-                    },
-                    onerror: error => {
-                        console.error(error);
-                        downloadImage();
                     }
                 });
+            } else {
+                document.title = `[${progress}/${Total}]`;
+                Button.textContent = `${language[7]}: [${progress}/${Total}]`;
+                progress++;
+                resolve();
             }
         });
     }
     for (let i = 0; i < Total; i++) {
         promises.push(Request(i));
-        await new Promise(resolve => setTimeout(resolve, 100));
+        await new Promise(resolve => setTimeout(resolve, 50));
     }
     await Promise.all(promises);
     Compression();
@@ -247,25 +273,172 @@ async function ZipDownload(Button, Folder, ImgData) {
                     }
                 }, (progress) => {
                     document.title = `${progress.percent.toFixed(1)} %`;
-                    Button.textContent = `壓縮封裝: ${progress.percent.toFixed(1)} %`;
+                    Button.textContent = `${language[8]}: ${progress.percent.toFixed(1)} %`;
                 }).then(zip => {
                     saveAs(zip, `${Folder}.zip`);
-                    Button.textContent = "壓縮完成";
+                    Button.textContent = language[9];
                     document.title = OriginalTitle;
-                    setTimeout(() => {Button.textContent = "壓縮下載"}, 4000);
-                    Button.disabled = false;
+                    setTimeout(() => {
+                        Button.textContent = ModeDisplay;
+                        Button.disabled = false;
+                    }, 3000);
                 }).catch(result => {
-                    Button.textContent = "壓縮失敗";
+                    Button.textContent = language[10];
                     document.title = OriginalTitle;
-                    setTimeout(() => {Button.textContent = "壓縮下載"}, 6000);
-                    Button.disabled = false;
+                    setTimeout(() => {
+                        Button.textContent = ModeDisplay;
+                        Button.disabled = false;
+                    }, 6000);
                 })
-            ]);
+            ])
         }
     }
 }
 
+/* 單圖下載 */
+async function ImageDownload(Button, Folder, ImgData) {
+    const Total = ImgData.size, promises = [];
+    let progress = 1, link, extension, retry=0;
+    async function Request(index) {
+        link = ImgData.get(index);
+        extension = GetExtension(link);
+        return new Promise((resolve) => {
+            if (link !== undefined) {
+                GM_download({
+                    url: link,
+                    name: `${Folder}_${(index + 1).toString().padStart(4, '0')}.${extension}`,
+                    headers : {"user-agent": navigator.userAgent},
+                    onload: () => {
+                        document.title = `[${progress}/${Total}]`;
+                        Button.textContent = `${language[7]}: [${progress}/${Total}]`;
+                        progress++;
+                        resolve();
+                    },
+                    onerror: () => {
+                        retry++;
+                        if (retry < 10) {
+                            Request(index);
+                        } else {resolve()}
+                    }
+                });
+            } else {
+                document.title = `[${progress}/${Total}]`;
+                Button.textContent = `${language[7]} [${progress}/${Total}]`;
+                progress++;
+                resolve();
+            }
+        });
+    }
+    for (let i = 0; i < Total; i++) {
+        promises.push(Request(i));
+    }
+    await Promise.all(promises);
+    Button.textContent = language[11];
+    setTimeout(() => {
+        Button.textContent = ModeDisplay;
+        Button.disabled = false;
+    }, 3000);
+}
+
+/* 下載模式切換 */
+async function DownloadModeSwitch() {
+    if (CompressMode){
+        GM_setValue("CompressedMode", false);
+    } else {
+        GM_setValue("CompressedMode", true);
+    }
+    location.reload();
+}
+
+/* 自適應css */
+function AdaptiveCSS(e, ex) {
+    const Domain = window.location.hostname;
+    if (Domain === "e-hentai.org") {
+        GM_addStyle(`${e}`);
+    } else if (Domain === "exhentai.org") {
+        GM_addStyle(`${ex}`);
+    }
+}
+
+/* work創建 */
 function BackgroundCreation() {
     let blob = new Blob([""], {type: "application/javascript"});
     return URL.createObjectURL(blob);
+}
+
+/* 顯示語言 */
+function display_language(language) {
+    let display = {
+        "zh-TW": [
+            "🔁 切換下載模式",
+            "壓縮下載",
+            "單圖下載",
+            "開始下載",
+            "未找到標題",
+            "獲取頁面",
+            "獲取連結",
+            "下載進度",
+            "壓縮封裝",
+            "壓縮完成",
+            "壓縮失敗",
+            "下載完成"
+        ],
+        "zh-CN": [
+            "🔁 切换下载模式",
+            "压缩下载",
+            "单图下载",
+            "开始下载",
+            "未找到标题",
+            "获取页面",
+            "获取链接",
+            "下载进度",
+            "压缩封装",
+            "压缩完成",
+            "压缩失败",
+            "下载完成"
+        ],
+        "ja": [
+            "🔁 ダウンロードモードの切り替え",
+            "圧縮ダウンロード",
+            "単一画像ダウンロード",
+            "ダウンロード開始",
+            "タイトルが見つかりませんでした",
+            "ページを取得する",
+            "リンクを取得する",
+            "ダウンロードの進捗状況",
+            "圧縮パッケージング",
+            "圧縮完了",
+            "圧縮に失敗しました",
+            "ダウンロードが完了しました"
+        ],
+        "en": [
+            "🔁 Switch download mode",
+            "Compressed download",
+            "Single image download",
+            "Start download",
+            "Title not found",
+            "Get page",
+            "Get link",
+            "Download progress",
+            "Compressed packaging",
+            "Compression complete",
+            "Compression failed",
+            "Download complete" 
+        ],
+        "ko": [
+            "🔁 다운로드 모드 전환",
+            "압축 다운로드",
+            "단일 이미지 다운로드",
+            "다운로드 시작",
+            "제목을 찾을 수 없습니다",
+            "페이지 가져오기",
+            "링크 가져오기",
+            "다운로드 진행 상황",
+            "압축 포장",
+            "압축 완료",
+            "압축 실패",
+            "다운로드 완료"
+        ]
+    };
+    return display[language] || display["en"];
 }
