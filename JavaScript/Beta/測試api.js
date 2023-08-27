7a5fb8170ed32b41f93f0ee3f23d9a8241acb912
TenshinoOtoKafu <s3459897@gmail.com>
2023-08-27 18:09:09 +0800

    測試修改

--- JavaScript/Beta/測試api.js
+++ JavaScript/Beta/測試api.js
@@ -1,7 +1,4 @@
-window.addEventListener('popstate', function(event) {
-    // event.state 包含了新的狀態信息
-    // 可以根據 event.state 的值執行特定操作
-    // 例如，你可以根據狀態信息加載不同的內容或執行其他操作
+/*window.addEventListener('popstate', function(event) {
     console.log('URL 地址變化:', window.location.href);
-});
+});*/
 // { capture: true } { passive: true }
