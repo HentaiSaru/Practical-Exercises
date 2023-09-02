e49b98f0653912f4b78f3c23a3fd1b16ca56eba2
TenshinoOtoKafu <s3459897@gmail.com>
2023-09-03 06:04:02 +0800

    修復失效規則

--- JavaScript/Release/TwitchBeautify.js
+++ JavaScript/Release/TwitchBeautify.js
@@ -4,7 +4,7 @@
 // @name:zh-CN   Twitch Beautify
 // @name:ja      Twitch Beautify
 // @name:en      Twitch Beautify
-// @version      0.0.18
+// @version      0.0.19
 // @author       HentaiSaru
 // @description         美化 Twitch 觀看畫面 , 懶人自動點擊 , 主頁自動暫停靜音自動播放影片
 // @description:zh-TW   美化 Twitch 觀看畫面 , 懶人自動點擊 , 主頁自動暫停靜音自動播放影片
@@ -19,76 +19,113 @@
 // @license      MIT
 // @namespace    https://greasyfork.org/users/989635
 
-// @run-at       document-end
+// @run-at       document-body
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @grant        GM_registerMenuCommand
 
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js
 // @resource     jui https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css
 // ==/UserScript==
 
 (function() {
-    const language = display_language(navigator.language);
-    var enabledstate;
+    var enabledstate, language = display_language(navigator.language);
+
+    /* ======================= 判斷是否調用美化 ========================= */
     if (GM_getValue("Beautify", [])) {
         enabledstate = language[1];
-        // 有時候會導致後續回首頁直播被暫停, 到底什麼奇怪的Bug
-        if (window.location.href === "https://www.twitch.tv/") {
-            PlayerAborted(true)
-        }
+        if (window.location.href === "https://www.twitch.tv/") {PlayerAborted(true)} // 首頁影片靜音
         main();
         setTimeout(DeleteFooter, 500);
     } else {
         enabledstate = language[0];
     }
     GM_registerMenuCommand(enabledstate, function() {Use()});
-})();
+    
+    /* ======================= 檢測美化觸發與恢復 (API) ========================= */
 
-/* 使用美化監聽 */
-async function main() {
-    const pattern = /^https:\/\/www\.twitch\.tv\/.+/, observer = new MutationObserver(() => {
-        if (pattern.test(window.location.href) && document.querySelector("video")) {
+    /* 使用美化監聽 */
+    async function main() {
+        const pattern = /^https:\/\/www\.twitch\.tv\/.+/, observer = new MutationObserver(() => {
+            if (pattern.test(window.location.href) && document.querySelector("video")) {
+                observer.disconnect();
+                BeautifyTrigger();
+                fun($("div[data-a-player-state='']"), false);
+            }
+        });
+        observer.observe(document.head, {childList: true, subtree: true});
+    }
+
+    /* 首頁恢復監聽 */
+    async function HomeRecovery(Nav, CB, CX) {
+        const observer = new MutationObserver(() => {
+            if (window.location.href === "https://www.twitch.tv/") {
+                observer.disconnect();
+                Nav.removeClass("Nav_Effect");
+                CX.removeClass("Channel_Expand_Effect");
+                CB.removeClass("button_Effect");
+                fun($("div[data-a-player-state='mini']"));
+                main();// 重新執行美化監聽
+            }
+        });
+        observer.observe(document.head, {childList: true, subtree: true});
+    }
+
+    /* 等待元素 */
+    async function WaitElem(selectors, timeout, callback) {
+        let timer, elements;
+        const observer = new MutationObserver(() => {
+            elements = selectors.map(selector => $(selector));
+            if (elements.every(element => element)) {
+                observer.disconnect();
+                clearTimeout(timer);
+                callback(elements);
+            }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+        timer = setTimeout(() => {
             observer.disconnect();
-            FindPlayPage();
-            fun($("div[data-a-player-state='']"), false);
-        }
-    });
-    observer.observe(document.head, {childList: true, subtree: true});
-}
+        }, timeout);
+    }
 
-/* 首頁恢復監聽 */
-async function HomeRecovery(Nav, CB, CX) {
-    const observer = new MutationObserver(() => {
-        if (window.location.href === "https://www.twitch.tv/") {
-            observer.disconnect();
-            Nav.removeClass("Nav_Effect");
-            CX.removeClass("Channel_Expand_Effect");
-            CB.removeClass("button_Effect");
-            fun($("div[data-a-player-state='mini']"));
-            main();// 重新執行美化監聽
+    /* 添加js */
+    async function addscript(Rule, ID="New-Add-script") {
+        let new_script = document.getElementById(ID);
+        if (!new_script) {
+            new_script = document.createElement("script");
+            new_script.id = ID;
+            document.head.appendChild(new_script);
         }
-    });
-    observer.observe(document.head, {childList: true, subtree: true});
-}
+        new_script.appendChild(document.createTextNode(Rule));
+    }
 
-/* 查找video頁面元素方法 */
-function FindPlayPage() {
-    let interval = setInterval(function() {
-        const [Channel, Collapsed_State, player, Nav, Channel_Button, Chat_button] = [
+    /* 導入樣式 */
+    GM_addStyle(`
+        ${GM_getResourceText("jui")}
+        .drag-border {
+            border: 2px solid white;
+            border-radius: 10px;
+        }
+    `);
+
+    /* ======================= 美化觸發 ========================= */
+
+    /* 查找video頁面元素 */
+    async function BeautifyTrigger() {
+        const Elem = [
+            "nav", // 導覽列
             ".side-nav", // 頻道元素
             ".simplebar-track.vertical", // 收合狀態
             "div[data-a-player-state='']", // 影片區塊
-            "nav.InjectLayout-sc-1i43xsx-0.ghHeNF", // 導覽列
             "button[data-a-target='side-nav-arrow']", // 頻道列 button
-            "button[data-a-target='right-column__toggle-collapse-btn']", // 聊天室 button
-        ].map(selector => $(selector));
-        const Channel_Parent = Channel.parent();
-        if (Nav[0] && player[0] && Chat_button[0] && Channel_Button[0] && Channel_Parent[0] && Collapsed_State[0]) {
-            clearInterval(interval);
+            "button[data-a-target='right-column__toggle-collapse-btn']" // 聊天室 button
+        ];
+        WaitElem(Elem, 8000, element => {
+            const [Nav, Channel, Collapsed_State, player, Channel_Button, Chat_button] = element;
+            const Channel_Parent = Channel.parent();
             // 判斷兩次總該打開了吧
             if (Collapsed_State.css("visibility") !== "visible") {Channel_Button.click()}
             if (Collapsed_State.css("visibility") === "hidden") {Channel_Button.click()}
@@ -98,200 +135,185 @@ function FindPlayPage() {
             PlayerAborted(false); // 恢復聲音
             ResumeWatching(); // 監聽恢復觀看
             HomeRecovery(Nav, Channel_Button, Channel_Parent); // 首頁復原監聽
-        }
-    }, 350);
-}
+        });
+    }
 
-/* 美化 */
-async function Beautify(Nav, play, CX) {
-    GM_addStyle(`
-        .Nav_Effect {
-            opacity: 0;
-            height: 1rem !important;
-            transition: opacity 0.5s , height 0.8s;
-        }
-        .Nav_Effect:hover {
-            opacity: 1;
-            height: 5rem !important;
-        }
-        .Channel_Expand_Effect {
-            opacity: 0;
-            width: 1rem;
-            transition: opacity 0.4s , width 0.7s;
-        }
-        .Channel_Expand_Effect:hover {
-            opacity: 1;
-            width: 24rem;
-        }
-    `);
-    play.css("z-index", "9999");
-    Nav.addClass("Nav_Effect");
-    CX.addClass("Channel_Expand_Effect");
-}
-
-/* 影片暫停和靜音 */
-async function PlayerAborted(control) {
-    let timeout=0, interval = setInterval(() => {
-        const player = document.querySelector("video");
-        if (player) {
-            if(control) {
-                player.muted = true;
-                if (!player.paused) {
-                    clearInterval(interval);
-                    player.pause();
-                } else {
-                    timeout++;
-                    if (timeout > 10) {
-                        clearInterval(interval);
-                    }
-                }
-            } else {
-                player.play();
-                player.muted = false;
-                if (player.muted) {
-                    clearInterval(interval);
-                    player.muted = false;
-                } else {
-                    timeout++;
-                    if (timeout > 30) {
-                        clearInterval(interval);
-                    }
-                }
+    /* 美化邏輯 */
+    async function Beautify(Nav, play, CX) {
+        GM_addStyle(`
+            .Nav_Effect {
+                opacity: 0;
+                height: 1rem !important;
+                transition: opacity 0.5s , height 0.8s;
             }
-        }
-    }, 500);
-}
+            .Nav_Effect:hover {
+                opacity: 1;
+                height: 5rem !important;
+            }
+            .Channel_Expand_Effect {
+                opacity: 0;
+                width: 1rem;
+                transition: opacity 0.4s , width 0.7s;
+            }
+            .Channel_Expand_Effect:hover {
+                opacity: 1;
+                width: 24rem;
+            }
+        `);
+        play.css("z-index", "9999");
+        Nav.addClass("Nav_Effect");
+        CX.addClass("Channel_Expand_Effect");
+    }
 
-/* 自動恢復觀看 */
-async function ResumeWatching() {
-    let recover;
-    const observer = new MutationObserver(() => {
-        try {recover = $("div[data-a-target='player-overlay-content-gate'] button")} catch {}
-        if (window.location.href === "https://www.twitch.tv/") {
-            observer.disconnect();
-        } else if (recover.length > 0) {
-            recover.click();
-        }
-    });
-    observer.observe($("div[data-a-player-state='']")[0], {childList: true, subtree: true});
-}
+    /* ======================= 附加功能 ========================= */
 
-/* 懶人自動點擊 */
-async function AutoClickC(Chat_button, Channel_Button) {
-    GM_addStyle(`
-        .button_Effect {
-            transform: translateY(10px);
-            color: rgba(239, 239, 241, 0.3) !important;
-        }
-        .button_Effect:hover {
-            color: rgb(239, 239, 241) !important;
-        }
-    `);
-    let timer, timer2;
-    Chat_button.addClass("button_Effect");
-    Channel_Button.addClass("button_Effect");
+    /* 自動恢復觀看 */
+    async function ResumeWatching() {
+        let recover;
+        const observer = new MutationObserver(() => {
+            try {recover = $("div[data-a-target='player-overlay-content-gate'] button")} catch {}
+            if (window.location.href === "https://www.twitch.tv/") {
+                observer.disconnect();
+            } else if (recover.length > 0) {
+                recover.click();
+            }
+        });
+        observer.observe($("div[data-a-player-state='']")[0], {childList: true, subtree: true});
+    }
 
-    Chat_button.on('mouseenter', function() {
-        timer = setTimeout(function() {
-            Chat_button.click();
-        }, 250);
-    });
-    Chat_button.on('mouseleave', function() {
+    /* 懶人自動點擊 */
+    async function AutoClickC(Chat_button, Channel_Button) {
+        GM_addStyle(`
+            .button_Effect {
+                transform: translateY(10px);
+                color: rgba(239, 239, 241, 0.3) !important;
+            }
+            .button_Effect:hover {
+                color: rgb(239, 239, 241) !important;
+            }
+        `);
+        let timer, timer2;
         Chat_button.addClass("button_Effect");
-        clearTimeout(timer);
-    });
-
-    Channel_Button.css("transform", "translateY(19px)");
-    Channel_Button.on('mouseenter', function() {
-        timer2 = setTimeout(function() {
-            Channel_Button.click();
-        }, 250);
-    });
-    Channel_Button.on('mouseleave', function() {
         Channel_Button.addClass("button_Effect");
-        clearTimeout(timer2);
-    });
-}
 
-GM_addStyle(`
-    ${GM_getResourceText("jui")}
-    .drag-border {
-        border: 2px solid white;
-        border-radius: 10px;
+        Chat_button.on('mouseenter', function() {
+            timer = setTimeout(function() {
+                Chat_button.click();
+            }, 250);
+        });
+        Chat_button.on('mouseleave', function() {
+            Chat_button.addClass("button_Effect");
+            clearTimeout(timer);
+        });
+
+        Channel_Button.css("transform", "translateY(19px)");
+        Channel_Button.on('mouseenter', function() {
+            timer2 = setTimeout(function() {
+                Channel_Button.click();
+            }, 250);
+        });
+        Channel_Button.on('mouseleave', function() {
+            Channel_Button.addClass("button_Effect");
+            clearTimeout(timer2);
+        });
     }
-`);
 
-/* 拖動添加 */
-async function fun(element, state=true) {
-    if (element.length > 0) {
-        if (state) {
-            element.draggable({ 
-                cursor: "grabbing",
-                start: function(event, ui) {
-                    $(this).find("div.ScAspectRatio-sc-18km980-1").addClass("drag-border");
-                },
-                stop: function(event, ui) {
-                    $(this).find("div.ScAspectRatio-sc-18km980-1").removeClass("drag-border");
+    /* 拖動添加 */
+    async function fun(element, state=true) {
+        if (element.length > 0) {
+            if (state) {
+                element.draggable({ 
+                    cursor: "grabbing",
+                    start: function(event, ui) {
+                        $(this).find("div.ScAspectRatio-sc-18km980-1").addClass("drag-border");
+                    },
+                    stop: function(event, ui) {
+                        $(this).find("div.ScAspectRatio-sc-18km980-1").removeClass("drag-border");
+                    }
+                });
+                element.resizable({
+                    handles: "all",
+                    minWidth: 50,
+                    minHeight: 50,
+                    aspectRatio: 16 / 10
+                });
+            } else {
+                if (element.data("ui-draggable")) {
+                    element.draggable("destroy");
+                    element.resizable("destroy");
                 }
-            });
-            element.resizable({
-                handles: "all",
-                minWidth: 50,
-                minHeight: 50,
-                aspectRatio: 16 / 10
-            });
-        } else {
-            if (element.data("ui-draggable")) {
-                element.draggable("destroy");
-                element.resizable("destroy");
             }
         }
     }
-}
 
-/* 刪除頁腳 */
-async function DeleteFooter() {
-    try {$("#twilight-sticky-footer-root").css("display", "none")} catch {}
-}
-
-/* 添加js */
-async function addscript(Rule, ID="New-Add-script") {
-    let new_script = document.getElementById(ID);
-    if (!new_script) {
-        new_script = document.createElement("script");
-        new_script.id = ID;
-        document.head.appendChild(new_script);
+    /* 影片暫停和靜音 */
+    async function PlayerAborted(control) {
+        let timeout=0, interval = setInterval(() => {
+            const player = document.querySelector("video");
+            if (player) {
+                if(control) {
+                    player.muted = true;
+                    if (!player.paused) {
+                        clearInterval(interval);
+                        player.pause();
+                    } else {
+                        timeout++;
+                        if (timeout > 50) {
+                            clearInterval(interval);
+                        }
+                    }
+                } else {
+                    player.play();
+                    player.muted = false;
+                    if (player.muted) {
+                        clearInterval(interval);
+                        player.muted = false;
+                    } else {
+                        timeout++;
+                        if (timeout > 50) {
+                            clearInterval(interval);
+                        }
+                    }
+                }
+            }
+        }, 500);
     }
-    new_script.appendChild(document.createTextNode(Rule));
-}
 
-/* 隨便寫的隱藏廣告, (測試) */
-async function AdProcessing() {
-    addscript(`
-        const interval = setInterval(() => {
-            document.querySelectorAll("iframe").forEach(iframe => {iframe.remove()});
-        }, 1500)
-    `, "ADB")
-    $("iframe").each(function() {$(this).remove()});
-}
-
-/* 使用設置開關 */
-function Use() {
-    if (GM_getValue("Beautify", [])) {
-        GM_setValue("Beautify", false);
-    } else {
-        GM_setValue("Beautify", true);
+    /* 隨便寫的隱藏廣告, (測試) */
+    async function AdProcessing() {
+        addscript(`
+            const interval = setInterval(() => {
+                document.querySelectorAll("iframe").forEach(iframe => {iframe.remove()});
+            }, 1500)
+        `, "ADB")
+        $("iframe").each(function() {$(this).remove()});
     }
-    location.reload();
-}
 
-function display_language(language) {
-    let display = {
-        "zh-TW": ["🛠️ 以禁用美化❌", "🛠️ 以啟用美化✅"],
-        "zh-CN": ["🛠️ 已禁用美化❌", "🛠️ 已启用美化✅"],
-        "ko": ["🛠️ 미화 비활성화❌", "🛠️ 미화 활성화✅"],
-        "ja": ["🛠️ 美化を無効にしました❌", "🛠️ 美化を有効にしました✅"],
-        "en-US": ["🛠️ Beautification disabled❌", "🛠️ Beautification enabled✅"],
-    };
-    return display[language] || display["en-US"];
-}
+    /* 刪除頁腳 */
+    async function DeleteFooter() {
+        try {$("#twilight-sticky-footer-root").css("display", "none")} catch {}
+    }
+
+    /* ======================= 切換/自適應 ========================= */
+
+    /* 使用設置開關 */
+    function Use() {
+        if (GM_getValue("Beautify", [])) {
+            GM_setValue("Beautify", false);
+        } else {
+            GM_setValue("Beautify", true);
+        }
+        location.reload();
+    }
+
+    function display_language(language) {
+        let display = {
+            "zh-TW": ["🛠️ 以禁用美化❌", "🛠️ 以啟用美化✅"],
+            "zh-CN": ["🛠️ 已禁用美化❌", "🛠️ 已启用美化✅"],
+            "ko": ["🛠️ 미화 비활성화❌", "🛠️ 미화 활성화✅"],
+            "ja": ["🛠️ 美化を無効にしました❌", "🛠️ 美化を有効にしました✅"],
+            "en-US": ["🛠️ Beautification disabled❌", "🛠️ Beautification enabled✅"],
+        };
+        return display[language] || display["en-US"];
+    }
+})();
