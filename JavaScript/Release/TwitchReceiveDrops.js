ac5fb372e3656b5eca742f234c23447c4291ade4
TenshinoOtoKafu <s3459897@gmail.com>
2023-09-08 13:30:52 +0800

    添加展示功能

--- JavaScript/Release/TwitchReceiveDrops.js
+++ JavaScript/Release/TwitchReceiveDrops.js
@@ -1,14 +1,16 @@
 // ==UserScript==
-// @name                Twitch Auto Receive Drops
-// @name:zh-TW          Twitch 自動收取 Drops
+// @name                Twitch 自動領取掉寶 / Auto Receive Drops
+// @name:zh-TW          Twitch 自動領取掉寶
+// @name:zh-CN          Twitch 自动领取掉宝
 // @name:en             Twitch Auto Receive Drops
-// @version             0.0.1
+// @version             0.0.2
 // @author              HentaiSaru
-// @description         Twitch 自動收取 Drops , 自訂檢查間隔
-// @description:zh-TW   Twitch 自動收取 Drops , 自訂檢查間隔
-// @description:en      Twitch automatically collects Drops, custom check interval
+// @description         Twitch 自動收取 [掉寶/Drops] , 簡易自訂設置 (優化版)
+// @description:zh-TW   Twitch 自動收取 [掉寶/Drops] , 簡易自訂設置 (優化版)
+// @description:zh-CN   Twitch 自动收取 [掉宝/Drops] , 简易自定义设置 (优化版)
+// @description:en      Twitch Auto Receive Drops, Easy Custom Settings (Optimized Version)
 
-// @match        https://www.twitch.tv/drops/inventory*
+// @match        https://www.twitch.tv/drops/inventory
 // @icon         https://cdn-icons-png.flaticon.com/512/8214/8214044.png
 
 // @license      MIT
@@ -18,14 +20,23 @@
 // ==/UserScript==
 
 (function() {
-    const custom = {
-        button: "button.ScCoreButton-sc-ocjdkq-0.ScCoreButtonPrimary-sc-ocjdkq-1",
-        CheckInterval: 60, // 檢查間隔 (seconds / 秒數)
+    var Withdraw, title;
+    /* 配置設定 */
+    const config = {
+        ProgressBar: "p.CoreText-sc-1txzju1-0.egOfyN span.CoreText-sc-1txzju1-0",
+        Checkbutton: "button.ScCoreButton-sc-ocjdkq-0.ScCoreButtonPrimary-sc-ocjdkq-1",
+        CheckInterval: 120, // 檢查間隔 (seconds / 秒數)
+        ProgressDisplay: true // 在標題展示進度
 
     }, observer = new MutationObserver(() => {
-        const claimButton = document.querySelector(custom.button);
-        claimButton ? (observer.disconnect(), claimButton.click()) : null;
+        Withdraw = document.querySelector(config.Checkbutton);
+        Withdraw ? (observer.disconnect(), Withdraw.click()) : null;
+
+        if (config.ProgressDisplay) {
+            title = `${document.querySelector(config.ProgressBar).textContent}%`;
+            title !== null ? document.title = title : null;
+        }
     });
     observer.observe(document.body, {childList: true, subtree: true});
-    setInterval(()=> {location.reload()}, 1000 * custom.CheckInterval);
+    setInterval(()=> {location.reload()}, 1000 * config.CheckInterval);
 })();
