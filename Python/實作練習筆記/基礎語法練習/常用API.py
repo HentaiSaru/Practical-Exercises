ea748b7018fc2b7139e6e57d90a95d7efb02138e
TenshinoOtoKafu <s3459897@gmail.com>
2023-10-02 22:20:41 +0800

    API

--- Python/實作練習筆記/基礎語法練習/常用API.py
+++ Python/實作練習筆記/基礎語法練習/常用API.py
@@ -26,11 +26,25 @@ a = "一個字串"
 print(f"字串的長度為 : {len(a)}")
 
 """
+Todo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Todo    常用 API
 
-*   isinstance(value, Type) # 判斷變數, 是否是指定的類型
+*   isinstance(value, Type) 判斷變數, 是否是指定的類型
 
-*   abs(value) # 取得數字的絕對值(正整數)
-*   hex(value) # 轉換16進位
-*   bin(value) # 轉換2進位
+*   abs(value) 取得數字的絕對值(正整數)
+*   hex(value) 轉換16進位
+*   bin(value) 轉換2進位
+
+?   列表類型數據 API
+
+*   list.add(value) 添加新元素 [用於set類型]
+*   list.union(value) 將兩個set合併, 回傳一個set排除重複 [用於set類型]
+*   list.update(value) 將 value 這個 set 插入, list 這個set中 [用於set類型]
+*   list.clear() 清空列表 [通用]
+*   list.copy() 複製列表, 回傳數據 [通用]
+*   list.difference() 回傳包含差異的 set [不確定]
+*   list.discard() 刪除指定的項目
+*   list.pop() 刪除一個元素
+*   list.remove() 刪除指定元素
+
 """
