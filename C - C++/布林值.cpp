92247d557e9a3294ca69379cd15955891bd63c04
TenshinoOtoKafu <s3459897@gmail.com>
2023-11-16 23:58:21 +0800

    布林值源碼

---
+++ C - C++/布林值.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h> // 導入布林套件
+
+int main() {
+    bool a = true;
+    bool b = false;
+
+    /*
+    *   1 = True
+    *   0 = False
+    */
+
+    printf("True: %d | False: %d", a, b);
+    // 比較輸出布林
+    printf("\n大小比較: %d", 10 > 9);
+}
