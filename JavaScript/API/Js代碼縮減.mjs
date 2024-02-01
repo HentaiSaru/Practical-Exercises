0d986cbdfe6966c0e455ea6a8dc548504b623b3c
TenshinoOtoKafu <s3459897@gmail.com>
2024-02-01 08:38:25 +0800

    多行註解邏輯消除

--- JavaScript/API/Js代碼縮減.mjs
+++ JavaScript/API/Js代碼縮減.mjs
@@ -1,6 +1,5 @@
 import path from "path";
 import open from "fs";
-import { log } from "console";
 
 class LoadData {
     constructor(input, output) {
@@ -10,55 +9,36 @@ class LoadData {
       this.s_advanced = false;
       this.e_advanced = true;
 
-      this.space = /^\r$/;
+      this.space = /^\s*\r$/;
       this.comment = /^\s*(\/\/|#).+\r$/;
       this.ms_comment = /^\s*(\/\*|""")/;
-      this.me_comment = /^\s*("""|\*\/\r)\s*$/;
+      this.me_comment = /^.*("""|\*\/\r)\s*$/;
 
       this.output_box = [];
       this.processed_box = [];
     }
 
     Advanced_analysis(data, n) {
-      if (this.ms_comment.test(data)) {
+      if (this.s_advanced && this.e_advanced) {
+        this.e_advanced = this.me_comment.test(data) ? false : true;
+
+      } else if (this.ms_comment.test(data) && !this.s_advanced) {
         this.s_advanced = true;
-        this.e_advanced = true;
 
-        if (this.me_comment.test(data)) {
-          this.s_advanced = false;
-          this.e_advanced = false;
-        }
+      } else if (this.me_comment.test(data) && this.e_advanced) {
+        this.e_advanced = false;
 
-        if (this.s_advanced && this.e_advanced) {
-          console.log(data);
-        } else if(this.s_advanced && !this.e_advanced) {
-          console.log(data);
-        }
-      } else if (this.s_advanced) {
-        console.log(data);
+      } else if (this.ms_comment.test(data) && this.me_comment.test(data)) {
+        this.s_advanced && this.e_advanced ? this.output_box.push(data) : null; // JS
+        // this.s_advanced && !this.e_advanced ? this.output_box.push(data) : null; // Python
+
       } else {
-        //console.log(data);
-      }
-
-      /*
-      1. 開頭 / 結尾 都是 true (刪除)
-      2. 開頭 是 結為 否 (刪除)
-      3. 開頭否 結尾 是 (不刪除)
-      */
-
-      //console.log(`[${n}] ${this.ms_comment.test(data)}/${this.me_comment.test(data)} : ${data}`);
-
-      /*if (this.s_advanced) {
-        if (this.me_comment.test(data)) {
-          this.advanced = false;
-        }
-      } else if (this.ms_comment.test(data)) {
-        this.advanced = true;
-      } else {
+        // console.log(`[${n}] ${this.ms_comment.test(data)}/${this.me_comment.test(data)} : ${data}`);
+        // console.log(`[${n}] ${this.s_advanced}/${this.e_advanced} : ${data}`);
         this.output_box.push(data);
-      }*/
+      }
     }
-  
+
     Read_data() {
       let test_number = 0;
       open.readFile(this.input, "utf-8", (err, data) => {
@@ -75,8 +55,8 @@ class LoadData {
             this.Advanced_analysis(line, ++test_number);
           }
         }
-        //console.log(this.output_box.join("\n"));
-        //this.Output_Data(this.output_box.join("\n"));
+
+        this.Output_Data(this.output_box.join("\n"));
       });
     }
 
@@ -93,8 +73,14 @@ class LoadData {
     }
 }
 
+// const Load = new LoadData(
+  // "E:/(新)下載點/TextEncryption.pyw",
+  // "E:/(新)下載點/TE.pyw"
+// );
+
 const Load = new LoadData(
-  "E:/(新)下載點/TextEncryption.pyw",
-  "E:/(新)下載點/TE.pyw"
+  "E:/(新)下載點/KemerEnhance.js",
+  "E:/(新)下載點/KE.js"
 );
+
 Load.Read_data();
