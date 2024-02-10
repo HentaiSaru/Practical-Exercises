742d202f4c02038dac0f18bcff3abbef97d44cc7
TenshinoOtoKafu <s3459897@gmail.com>
2024-02-10 20:26:46 +0800

    測試修正

--- JavaScript/Experiment/Kemer下載器_Json處理.js
+++ JavaScript/Experiment/Kemer下載器_Json處理.js
@@ -1,8 +1,8 @@
-const fs = require("fs")
+import { readFile } from "fs";
 
-const json_path = "E:/(新)下載點/toone.json"
+const json_path = "E:/(新)下載點/MANA.json"
 
-fs.readFile(json_path, "utf-8", (err, data)=> {
+readFile(json_path, "utf-8", (err, data)=> {
     for (const value of Object.values(JSON.parse(data))) {
         try {
             for (const [name, link] of Object.entries(value["下載連結"])) {
