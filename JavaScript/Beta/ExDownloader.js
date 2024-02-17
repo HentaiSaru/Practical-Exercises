61780144d22e6a3ba6b2322a589685a9c15f1a55
TenshinoOtoKafu <s3459897@gmail.com>
2024-02-18 02:49:41 +0800

    修改填充尾數邏輯

--- JavaScript/Beta/ExDownloader.js
+++ JavaScript/Beta/ExDownloader.js
@@ -60,7 +60,7 @@
             this.Ex = /https:\/\/exhentai\.org\/g\/\d+\/[a-zA-Z0-9]+/;
             this.Ran = (u) => {return this.E.test(u) || this.Ex.test(u)}
             this.Css = (a, e, ex) => {
-                let css = location.hostname == "exhentai.org" ? ex : e;
+                let css = location.hostname != "exhentai.org" ? e : ex;
                 addstyle(`${a}${css}`, "button-style");
             }
         }
@@ -209,6 +209,14 @@
             this.WorkerCreation = (code) => {
                 let blob = new Blob([code], {type: "application/javascript"});
                 return new Worker(URL.createObjectURL(blob));
+            }
+            /* 取得填充值(最少填充 2 個 0) */
+            this.FillValue = (page) => {
+                return Math.max(2, `${page}`.length);
+            }
+            /* 取得尾數(使用 0 填充) */
+            this.Mantissa = (str, fill) => {
+                return `${++str}`.padStart(fill, "0");
             }
         }
 
@@ -408,12 +416,10 @@
         /* 壓縮下載 */
         async __ZipDownload(Button, Folder, ImgData) {
             const Data = new JSZip(), self = this, Total = ImgData.size;
-            let progress = 1, thread = this.Download_IT, delay = this.Download_ID, Fill = Total.toString().length;
-            let time, link, mantissa, extension;
+            let time, link, progress = 1, thread = self.Download_IT, delay = self.Download_ID, Fill = self.FillValue(Total);
             async function Request(index, retry) {
                 time = Date.now();
                 link = ImgData.get(index);
-                extension = self.Extension(link);
                 return new Promise((resolve, reject) => {
                     if (typeof link !== "undefined") {
                         GM_xmlhttpRequest({
@@ -424,8 +430,7 @@
                             onload: response => {
                                 if (response.status === 200 && response.response instanceof Blob && response.response.size > 0) {
                                     [ delay, thread ] = self.Dynamic(time, delay, thread, self.Download_ND);
-                                    mantissa = (index + 1).toString().padStart(Fill, "0");
-                                    Data.file(`${Folder}/${mantissa}.${extension}`, response.response);
+                                    Data.file(`${Folder}/${self.Mantissa(index, Fill)}.${self.Extension(link)}`, response.response);
                                     document.title = `[${progress}/${Total}]`;
                                     Button.textContent = `${language.DS_04}: [${progress}/${Total}]`;
                                     progress++;
@@ -476,18 +481,15 @@
         /* 單圖下載 */
         async __ImageDownload(Button, Folder, ImgData) {
             const Total = ImgData.size, self = this;
-            let progress = 1, thread = this.Download_IT, delay = this.Download_ID, Fill = Total.toString().length;
-            let time, link, extension;
+            let time, link, progress = 1, thread = self.Download_IT, delay = self.Download_ID, Fill = self.FillValue(Total);
             async function Request(index, retry) {
                 time = Date.now();
                 link = ImgData.get(index);
-                extension = self.Extension(link);
                 return new Promise((resolve, reject) => {
                     if (typeof link !== "undefined") {
                         GM_download({
                             url: link,
-                            name: `${Folder} ${(index + 1).toString().padStart(Fill, "0")}.${extension}`,
-                            headers : {"user-agent": navigator.userAgent},
+                            name: `${Folder}-${self.Mantissa(index, Fill)}.${self.Extension(link)}`,
                             onload: () => {
                                 [ delay, thread ] = self.Dynamic(time, delay, thread, self.Download_ND);
                                 document.title = `[${progress}/${Total}]`;
@@ -621,73 +623,33 @@
         let display = {
             "zh-TW": [{
                 "MN_01" : "🔁 切換下載模式",
-                "DM_01" : "壓縮下載",
-                "DM_02" : "單圖下載",
-                "DM_03" : "下載中鎖定",
-                "DS_01" : "開始下載",
-                "DS_02" : "獲取頁面",
-                "DS_03" : "獲取連結",
-                "DS_04" : "下載進度",
-                "DS_05" : "壓縮封裝",
-                "DS_06" : "壓縮完成",
-                "DS_07" : "壓縮失敗",
-                "DS_08" : "下載完成"
+                "DM_01" : "壓縮下載", "DM_02" : "單圖下載", "DM_03" : "下載中鎖定",
+                "DS_01" : "開始下載", "DS_02" : "獲取頁面", "DS_03" : "獲取連結", "DS_04" : "下載進度",
+                "DS_05" : "壓縮封裝", "DS_06" : "壓縮完成", "DS_07" : "壓縮失敗", "DS_08" : "下載完成"
             }],
             "zh-CN": [{
                 "MN_01" : "🔁 切换下载模式",
-                "DM_01" : "压缩下载",
-                "DM_02" : "单图下载",
-                "DM_03" : "下载中锁定",
-                "DS_01" : "开始下载",
-                "DS_02" : "获取页面",
-                "DS_03" : "获取链接",
-                "DS_04" : "下载进度",
-                "DS_05" : "压缩封装",
-                "DS_06" : "压缩完成",
-                "DS_07" : "压缩失败",
-                "DS_08" : "下载完成"
+                "DM_01" : "压缩下载", "DM_02" : "单图下载", "DM_03" : "下载中锁定",
+                "DS_01" : "开始下载", "DS_02" : "获取页面", "DS_03" : "获取链接", "DS_04" : "下载进度",
+                "DS_05" : "压缩封装", "DS_06" : "压缩完成", "DS_07" : "压缩失败", "DS_08" : "下载完成"
             }],
             "ja": [{
                 "MN_01" : "🔁 ダウンロードモードの切り替え",
-                "DM_01" : "圧縮ダウンロード",
-                "DM_02" : "単一画像ダウンロード",
-                "DM_03" : "ダウンロード中にロックされました",
-                "DS_01" : "ダウンロード開始",
-                "DS_02" : "ページを取得する",
-                "DS_03" : "リンクを取得する",
-                "DS_04" : "ダウンロードの進捗状況",
-                "DS_05" : "圧縮パッケージング",
-                "DS_06" : "圧縮完了",
-                "DS_07" : "圧縮に失敗しました",
-                "DS_08" : "ダウンロードが完了しました"
+                "DM_01" : "圧縮ダウンロード", "DM_02" : "単一画像ダウンロード", "DM_03" : "ダウンロード中にロックされました",
+                "DS_01" : "ダウンロード開始", "DS_02" : "ページを取得する", "DS_03" : "リンクを取得する", "DS_04" : "ダウンロードの進捗状況",
+                "DS_05" : "圧縮パッケージング", "DS_06" : "圧縮完了", "DS_07" : "圧縮に失敗しました", "DS_08" : "ダウンロードが完了しました"
             }],
             "en-US": [{
                 "MN_01" : "🔁 Switch download mode",
-                "DM_01" : "Compressed download",
-                "DM_02" : "Single image download",
-                "DM_03" : "Downloading Locked",
-                "DS_01" : "Start download",
-                "DS_02" : "Get page",
-                "DS_03" : "Get link",
-                "DS_04" : "Download progress",
-                "DS_05" : "Compressed packaging",
-                "DS_06" : "Compression complete",
-                "DS_07" : "Compression failed",
-                "DS_08" : "Download complete"
+                "DM_01" : "Compressed download", "DM_02" : "Single image download", "DM_03" : "Downloading Locked",
+                "DS_01" : "Start download", "DS_02" : "Get page", "DS_03" : "Get link", "DS_04" : "Download progress",
+                "DS_05" : "Compressed packaging", "DS_06" : "Compression complete", "DS_07" : "Compression failed", "DS_08" : "Download complete"
             }],
             "ko": [{
                 "MN_01" : "🔁 다운로드 모드 전환",
-                "DM_01" : "압축 다운로드",
-                "DM_02" : "단일 이미지 다운로드",
-                "DM_03" : "다운로드 중 잠김",
-                "DS_01" : "다운로드 시작",
-                "DS_02" : "페이지 가져오기",
-                "DS_03" : "링크 가져오기",
-                "DS_04" : "다운로드 진행 상황",
-                "DS_05" : "압축 포장",
-                "DS_06" : "압축 완료",
-                "DS_07" : "압축 실패",
-                "DS_08" : "다운로드 완료"
+                "DM_01" : "압축 다운로드", "DM_02" : "단일 이미지 다운로드", "DM_03" : "다운로드 중 잠김",
+                "DS_01" : "다운로드 시작", "DS_02" : "페이지 가져오기", "DS_03" : "링크 가져오기", "DS_04" : "다운로드 진행 상황",
+                "DS_05" : "압축 포장", "DS_06" : "압축 완료", "DS_07" : "압축 실패", "DS_08" : "다운로드 완료"
             }]
         };
 
