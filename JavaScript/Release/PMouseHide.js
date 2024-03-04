2b367ad673e00b53a3324a70abffbbb55b021c52
TenshinoOtoKafu <s3459897@gmail.com>
2024-03-04 09:21:38 +0800

    修改描述

--- JavaScript/Release/PMouseHide.js
+++ JavaScript/Release/PMouseHide.js
@@ -8,12 +8,12 @@
 // @version      0.0.3
 // @author       HentaiSaru
 
-// @description         電腦觀看影片時，如果滑鼠停留在影片區域內一段時間，則隱藏滑鼠遊標和進度條，當滑鼠再次移動時將重新顯示。隱藏功能僅在滑鼠位於影片區域內時觸發。 手機觀看影片時，向右滑動手指可加速影片播放，滑動距離越長，加速效果越明顯，最高可達16倍速。釋放手指後，影片將恢復到原始播放速度。此加速功能僅在觸碰影片區域時生效。
-// @description:zh-TW   電腦觀看影片時，如果滑鼠停留在影片區域內一段時間，則隱藏滑鼠遊標和進度條，當滑鼠再次移動時將重新顯示。隱藏功能僅在滑鼠位於影片區域內時觸發。 手機觀看影片時，向右滑動手指可加速影片播放，滑動距離越長，加速效果越明顯，最高可達16倍速。釋放手指後，影片將恢復到原始播放速度。此加速功能僅在觸碰影片區域時生效。
-// @description:zh-CN   电脑观看视频时，鼠标停留在视频区域一段时间后，会隐藏鼠标指针和进度条，当鼠标再次移动时会重新显示。隐藏功能仅在鼠标位于视频区域内时触发。手机观看视频时，向右滑动手指可加速视频播放，滑动距离越长，加速效果越明显，最高可达16倍速。松开手指后，视频将恢复到原始播放速度。此加速功能仅在触摸视频区域时生效。
-// @description:en      When watching videos on a computer, if the mouse stays within the video area for a while, the mouse cursor and progress bar will be hidden. They will reappear when the mouse moves again. The hiding function is triggered only when the mouse is within the video area. When watching videos on a mobile phone, swiping the finger to the right accelerates video playback. The longer the swipe, the more noticeable the acceleration effect, up to 16 times speed. Upon releasing the finger, the video will return to the original playback speed. This acceleration function only works when touching the video area.
-// @description:ja      コンピュータで動畫を視聽している場合、マウスが一定時間動畫領域內に滯在すると、マウスカーソルと進行バーが非表示になります。マウスが再度移動すると再表示されます。非表示機能は、マウスが動畫領域內にある場合にのみトリガーされます。攜帯電話で動畫を視聽している場合、指を右にスワイプすると動畫の再生が加速します。スワイプの長さが長いほど、加速効果がより顕著になり、最大16倍速までです。指を離すと、動畫は元の再生速度に戻ります。この加速機能は、動畫領域に觸れている場合のみ機能します。
-// @description:ko      컴퓨터에서 비디오를 시청하는 경우, 마우스가 비디오 영역에 일정 시간 머무를 경우 마우스 커서와 진행 막대가 숨겨집니다. 마우스가 다시 움직이면 다시 표시됩니다. 숨기기 기능은 비디오 영역 내에 마우스가 있을 때만 트리거됩니다. 핸드폰에서 비디오를 시청하는 경우, 손가락을 오른쪽으로 스와이프하면 비디오 재생이 가속화됩니다. 스와이프가 길면 가속 효과가 더욱 뚜렷해지며 최대 16배 속도까지 가능합니다. 손가락을 놓으면 비디오는 원래의 재생 속도로 돌아갑니다. 이 가속 기능은 비디오 영역을 터치할 때만 작동합니다.
+// @description         電腦端滑鼠於影片區塊上停留一段時間，會隱藏滑鼠遊標和進度條，滑鼠再次移動時將重新顯示，收機端在影片區塊向右滑，會觸發影片加速，滑越多加越多最高16倍，放開後恢復正常速度。
+// @description:zh-TW   電腦端滑鼠於影片區塊上停留一段時間，會隱藏滑鼠遊標和進度條，滑鼠再次移動時將重新顯示，收機端在影片區塊向右滑，會觸發影片加速，滑越多加越多最高16倍，放開後恢復正常速度。
+// @description:zh-CN   电脑端在观看视频时，鼠标停留在视频区域一段时间后，鼠标指针和进度条会被隐藏。当鼠标再次移动时，它们将重新显示。手机端观看视频时，在视频区域向右滑动手指会触发视频加速，滑动越多，加速效果越明显，最高可达16倍。释放手指后，视频将恢复正常速度。
+// @description:en      On desktop, when the mouse hovers over the video for a while, the mouse cursor and progress bar will disappear. They will reappear when the mouse moves again. On mobile, swiping right in the video area triggers video acceleration, with more swipes resulting in faster acceleration, up to 16 times. Releasing the finger restores normal playback speed.
+// @description:ja      デスクトップでは、マウスが動畫上にしばらく停止すると、マウスカーソルと進行バーが非表示になります。マウスが再度移動すると再表示されます。モバイル端末では、動畫エリアで右にスワイプすると、動畫の加速がトリガーされます。より多くスワイプすると、最大16倍までの加速が得られます。指を離すと、通常の再生速度に戻ります。
+// @description:ko      데스크톱에서 비디오를 시청할 때 마우스가 비디오 위에 일정 시간 머물면 마우스 커서와 진행 막대가 숨겨집니다. 마우스가 다시 움직일 때 다시 나타납니다. 휴대폰에서 비디오 영역에서 오른쪽으로 스와이프하면 비디오가 가속됩니다. 스와이프할수록 더 빠른 가속이 발생하며 최대 16 배까지 가능합니다. 손가락을 놓으면 일반 재생 속도로 돌아갑니다.
 
 // @match        *://*.pornhub.com/view_video.php?viewkey=*
 // @match        *://*.pornhubpremium.com/view_video.php?viewkey=*
