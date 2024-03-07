1c6d2db7b50a5e05f3e1b7a8ce8f4d9a3113ea9f
TenshinoOtoKafu <s3459897@gmail.com>
2024-03-07 13:06:19 +0800

    修改 api

--- JavaScript/Beta/GrammarSimplified.js
+++ JavaScript/Beta/GrammarSimplified.js
@@ -24,15 +24,9 @@
  */
 class API {
     constructor() {
+        this.Buffer = document.createDocumentFragment();
         this.ListenerRecord = new Map();
         this.Parser = new DOMParser();
-        this.GM = {
-            __verify: val => val !== undefined ? val : null,
-            set: function(val, put) {GM_setValue(val, put)},
-            get: function(val, call) {return this.__verify(GM_getValue(val, call))},
-            setjs: function(val, put) {GM_setValue(val, JSON.stringify(put, null, 4))},
-            getjs: function(val, call) {return JSON.parse(this.__verify(GM_getValue(val, call)))},
-        };
         this.Template = {
             log: label=> console.log(label),
             warn: label=> console.warn(label),
@@ -84,9 +78,10 @@ class API {
      */
     ExtensionName(link) {
         try {
-            const match = link.match(/\.([^.]+)$/);
-            return match[1].toLowerCase() || "png";
-        } catch {return "png"}
+            return link.match(/\.([^.]+)$/)[1].toLowerCase() || "png";
+        } catch {
+            return "png";
+        }
     }
 
     /**
@@ -309,24 +304,32 @@ class API {
     /* ========== 油猴的 API ========== */
 
     /**
-     * * { 操作存储空間 }
+     ** { 操作存储空間 (精簡版) }
+     * 
      * // @grant GM_setValue
      * // @grant GM_getValue
-     * @param {string} operate - 操作類型 ("set", "get", "setjs", "getjs")
+     * // @grant GM_listValues
+     * // @grant GM_deleteValue
+     * 
+     * @param {string} operate - 操作類型 ("del", "all", "set", "get", "sjs", "gjs")
      * @param {string} key     - 操作數據索引Key
      * @param {*} orig         - 原始的回傳值
      * @returns {data}         - 獲取的數據值
-     * 
+     *
      * @example
-     * 數據 A = store("get", "資料A")
+     * 數據A = store("get", "資料A")
      * store("setjs", "資料B", "數據B")
      */
-    store(operate, key, orig=null){
-        switch (operate[0]) {
-            case "g": return this.GM[operate](key, orig);
-            case "s": return orig !== null ? this.GM[operate](key, orig) : null;
-            default: return new Error("wrong type of operation");
-        }
+    store(operation, key=null, value=null) {
+        const verify = val => (val !== void 0 ? val : null);
+        return {
+            del: (key) => GM_deleteValue(key),
+            all: () => verify(GM_listValues()),
+            set: (key, value) => GM_setValue(key, value),
+            get: (key, defaultValue) => verify(GM_getValue(key, defaultValue)),
+            sjs: (key, value) => GM_setValue(key, JSON.stringify(value, null, 4)),
+            gjs: (key, defaultValue) => JSON.parse(verify(GM_getValue(key, defaultValue)))
+        }[operation](key, value);
     }
 
     /**
