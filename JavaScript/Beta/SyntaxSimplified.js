c92fde6fe701371efbe86da42e78a94b1a13eb3f
TenshinoOtoKafu <s3459897@gmail.com>
2024-04-11 20:59:46 +0800

    句法更新

--- JavaScript/Beta/SyntaxSimplified.js
+++ JavaScript/Beta/SyntaxSimplified.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         SyntaxSimplified
-// @version      2024/04/10
+// @version      2024/04/11
 // @author       Canaan HS
 // @description  Library for simplifying code logic and syntax
 // @namespace    https://greasyfork.org/users/989635
