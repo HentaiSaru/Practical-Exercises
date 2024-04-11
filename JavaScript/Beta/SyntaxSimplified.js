441ff4d34591e4961ab8ec70a80adb56181cb7e3
TenshinoOtoKafu <s3459897@gmail.com>
2024-04-11 22:59:40 +0800

    函數簡化修正

--- JavaScript/Beta/SyntaxSimplified.js
+++ JavaScript/Beta/SyntaxSimplified.js
@@ -36,7 +36,7 @@ class Syntax {
         };
         this.query = {
             Match: /[ .#=:]/,
-            "#": (source, select, all) => source.getElementById(select.slice(1)),
+            "#": (source, select) => source.getElementById(select.slice(1)),
             ".": (source, select, all) => {
                 const query = source.getElementsByClassName(select.slice(1));
                 return all ? Array.from(query) : query[0];
@@ -51,7 +51,7 @@ class Syntax {
                 : source.querySelector(select);
             }
         };
-        this.formula = {
+        this.StorageMatch = {
             Type: (parse) => Object.prototype.toString.call(parse).slice(8, -1),
             String: (storage, key, value) =>
                 value != null ? (storage.setItem(key, JSON.stringify(value)), true) : JSON.parse(key),
@@ -73,18 +73,18 @@ class Syntax {
 
     /**
      * * { 簡化查找語法 }
-     * @param {string} Selector - 查找元素
-     * @param {boolean} All     - 是否查找全部
-     * @param {element} Source  - 查找來源
+     * @param {string} selector - 查找元素
+     * @param {boolean} {all}   - 是否查找全部
+     * @param {element} {root}  - 查找來源
      * @returns {element}       - DOM 元素
      * 
      * @example
-     * $$("查找元素", {all:true, source:變更來源})
+     * $$("查找元素", {all: true, root: 查找來源})
      */
-    $$(select, {all=false, source=document}={}) {
-        const type = !this.query.Match.test(select) ? "tag"
-        : this.query.Match.test(select.slice(1)) ? "default" : select[0];
-        return this.query[type](source, select, all);
+    $$(selector, {all=false, root=document}={}) {
+        const type = !this.query.Match.test(selector) ? "tag"
+        : this.query.Match.test(selector.slice(1)) ? "default" : selector[0];
+        return this.query[type](root, selector, all);
     }
 
     /**
@@ -94,6 +94,15 @@ class Syntax {
      */
     DomParse(html) {
         return this.Parser.parseFromString(html, "text/html");
+    }
+
+    /**
+     * * { 排除不能用做檔名的字串 }
+     * @param {string} name - 要修正的字串
+     * @returns {string}    - 排除後的字串
+     */
+    NameFilter(name) {
+        return name.replace(/[\/\?<>\\:\*\|":]/g, "");
     }
 
     /**
@@ -110,26 +119,6 @@ class Syntax {
     }
 
     /**
-     ** { 回傳下載圖片的尾數 }
-     * @param {number} index   - 圖片的頁數
-     * @param {number} padding - 填充量 [由 GetFill() 取得填充量]
-     * @param {string} filler  - 用於填充的字串
-     * @returns {string}       - 經填充後的尾數
-     */
-    Mantissa(index, padding, filler="0") {
-        return `${++index}`.padStart(padding, filler);
-    }
-
-    /**
-     * * { 排除不能用做檔名的字串 }
-     * @param {string} name - 要變更的字串
-     * @returns {string}    - 變更後的字串
-     */
-    IllegalCharacters(name) {
-        return name.replace(/[\/\?<>\\:\*\|":]/g, "");
-    }
-
-    /**
      * * { 解析網址字串的副檔名 }
      * @param {string} link - 含有副檔名的連結
      * @returns {string}    - 回傳副檔名字串
@@ -140,6 +129,20 @@ class Syntax {
         } catch {
             return "png";
         }
+    }
+
+    /**
+     ** { 回傳下載圖片的尾數 }
+     * @param {number} index   - 圖片的頁數
+     * @param {number} padding - 填充量 [由 GetFill() 取得填充量]
+     * @param {string} filler  - 用於填充的字串
+     * @param {string} type    - 圖片的副檔名, 輸入圖片的連結
+     * @returns {string}       - 經填充後的尾數
+     */
+    Mantissa(index, padding, filler="0", type=null) {
+        return type ?
+            `${++index}.${this.ExtensionName(type)}`.padStart(padding, filler)
+            : `${++index}`.padStart(padding, filler);
     }
 
     /**
@@ -157,7 +160,7 @@ class Syntax {
      * @param {Integer} delay - 延遲毫秒
      * @returns { Promise }
      */
-    sleep (delay) {
+    sleep(delay) {
         return new Promise(resolve => setTimeout(resolve, delay));
     }
 
@@ -210,8 +213,8 @@ class Syntax {
 
     /**
      * * { 刪除 監聽器 }
-     * @param {string} element  - 添加元素
-     * @param {string} type     - 監聽器類型
+     * @param {string} element - 添加元素
+     * @param {string} type    - 監聽器類型
      */
     async RemovListener(element, type) {
         const Listen = this.ListenerRecord[element]?.[type];
@@ -245,17 +248,15 @@ class Syntax {
 
     /**
      ** { 持續監聽對象, 並運行函數 (用於持續監聽, 無清除) }
-     *
-     * @param {element} object - 觀察對象
-     * @param {function} trigger - 觸發函數
-     * @param {object}
-     * mark - 創建標記, 用於避免重複創建
-     * subtree - 觀察 目標節點及其所有後代節點的變化
-     * childList - 觀察 目標節點的子節點數量的變化
-     * characterData - 觀察 目標節點的屬性值的變化
+     * @param {element} object          - 觀察對象
+     * @param {function} trigger        - 觸發函數
+     * @param {string} {mark}           - 創建標記, 用於避免重複創建
+     * @param {boolean} {subtree}       - 觀察 目標節點及其所有後代節點的變化
+     * @param {boolean} {childList}     - 觀察 目標節點的子節點數量的變化
+     * @param {boolean} {characterData} - 觀察 目標節點的屬性值的變化
      * 
      * @example
-     * Observer("", ()=> {}, {mark: "創建", childList: false, characterData: true})
+     * Observer("", ()=> {}, {mark: "標記", childList: false, characterData: true})
      */
     async Observer(object, trigger, {
         mark=false,
@@ -276,19 +277,18 @@ class Syntax {
 
     /**
      * * { 等待元素出現 }
-     * 
-     * @param {string} selector     - 等待元素
-     * @param {boolean} all         - 是否多選
-     * @param {number} timeout      - 等待超時 (秒數)
-     * @param {function} callback   - 回條函式
-     * @param {Element} object      - 監聽的 DOM 物件 (預設 body)
-     * @param {number} throttle     - 對監聽觸發進行節流 (毫秒)
+     * @param {string} selector   - 等待元素
+     * @param {boolean} all       - 是否多選
+     * @param {number} timeout    - 等待超時 (秒數)
+     * @param {function} callback - 回條函式
+     * @param {Element} {object}  - 監聽的 DOM 物件 (預設 body)
+     * @param {number} {throttle} - 對監聽觸發進行節流 (毫秒)
      * 
      * @example
      * WaitElem("元素", false, 1, call => {
-     *      後續操作...
+     *      全部找到... | 超時過後...
      *      console.log(call);
-     * }, {object:document, throttle:100})
+     * }, {object: document, throttle: 100})
      */
     async WaitElem(selector, all, timeout, callback, {object=document.body, throttle=0}={}) {
         let timer, element, result;
@@ -313,18 +313,17 @@ class Syntax {
 
     /**
      * * { 等待元素出現 (Map 物件版) }
-     * 
-     * @param {string} selector     - 等待元素
-     * @param {number} timeout      - 等待超時 (秒數)
-     * @param {function} callback   - 回條函式
-     * @param {Element} object      - 監聽的 DOM 物件 (預設 body)
-     * @param {number} throttle     - 對監聽觸發進行節流 (毫秒)
+     * @param {string} selector   - 等待元素
+     * @param {number} timeout    - 等待超時 (秒數)
+     * @param {function} callback - 回條函式
+     * @param {Element} {object}  - 監聽的 DOM 物件 (預設 body)
+     * @param {number} {throttle} - 對監聽觸發進行節流 (毫秒)
      * 
      * @example
      * WaitElem(["元素1", "元素2", "元素3"], 等待時間(秒), call => {
-     *      全部找到後續操作...
+     *      全部找到... | 超時過後...
      *      const [元素1, 元素2, 元素3] = call;
-     * }, {object:document, throttle:100})
+     * }, {object: document, throttle: 100})
      */
     async WaitMap(selectors, timeout, callback, {object=document.body, throttle=0}={}) {
         let timer, elements;
@@ -363,7 +362,9 @@ class Syntax {
     /**
      ** { 獲取運行經過時間 }
      * @param {Date.now()} time - 傳入 Date.now()
-     * @param {boolean} log - 是否直接打印
+     * @param {string} show - 顯示的說明文字
+     * @param {string} {style} - 展示的風格
+     * @param {boolean} {log} - 是否直接打印
      * 
      * @returns {Date.now()}
      * 
@@ -375,9 +376,9 @@ class Syntax {
      * let start = Runtime();
      * Runtime(start, true);
      */
-    Runtime(time=null, show="Elapsed Time:", log=true) {
+    Runtime(time=null, show="Elapsed Time:", {style="\x1b[1m\x1b[36m%s\x1b[0m", log=true}={}) {
         return !time? Date.now(): log?
-        console.log("\x1b[1m\x1b[36m%s\x1b[0m", `${show} ${((Date.now()-time)/1e3)}s`):
+        console.log(style, `${show} ${((Date.now()-time)/1e3)}s`):
         (Date.now() - time);
     }
 
@@ -410,7 +411,7 @@ class Syntax {
     }
 
     /**
-     ** { 節流函數, 立即觸發, 後續按照指定的速率運行, 期間多餘的觸發將會被丟棄 }
+     ** { 節流函數, 立即觸發, 後續按照指定的速率運行, 期間多餘的觸發將會被忽略 }
      * @param {function} func - 要觸發的函數
      * @param {number} delay - 延遲的時間ms
      * @returns {function}
@@ -432,18 +433,18 @@ class Syntax {
     /**
      * 解析範圍進行設置 (索引從 1 開始)
      * 
-     * @param {string} scope - 設置的索引範圍 [1, 2, 3-5, 6~10, -4, !8]
-     * @param {object} obj   - 需要設置範圍的物件
-     * @returns {object} - 回傳設置完成的物件
+     * @param {string} scope  - 設置的索引範圍 [1, 2, 3-5, 6~10, -4, !8]
+     * @param {object} object - 需要設置範圍的物件
+     * @returns {object}      - 回傳設置完成的物件
      * 
      * @example
      * object = ScopeParsing("", object);
      */
-    ScopeParsing(scope, obj) {
+    ScopeParsing(scope, object) {
         const // 使用 set 避免重複索引
             result = new Set(),
             exclude = new Set(),
-            len = obj.length;
+            len = object.length;
         for (const str of scope.split(/\s*,\s*/)) { // 使用 , 進行分割 , 的前後可有任意空格
             // 取索引值 -1 是為了得到真正的索引值
             if (/^\d+$/.test(str)) { // 單數字
@@ -472,25 +473,26 @@ class Syntax {
 
     /**
      ** { 瀏覽器 Storage 操作 }
-     * @param {sessionStorage | localStorage} storage - 存儲的類型 
      * @param {string} key - 存儲的 key 值
-     * @param {*} value - 存儲的 value 值
-     * @returns {boolean | *} - 回傳保存的值
+     * @param {sessionStorage | localStorage} {type} - 存儲的類型 (預設 sessionStorage)
+     * @param {*} {value}  - 存儲的 value 值
+     * @param {*} {error}  - 當不存在此值是要回傳的
+     * @returns {*}        - 回傳保存的值
      * 
      * @example
      * 支援的類型 (String, Number, Array, Object, Boolean, Date, Map)
-     * 
-     *  Storage("會話數據", {value: 123})
-     *  Storage("會話數據")
-     * 
-     *  Storage("本地數據", {value: 123, storage: localStorage})
-     *  Storage("本地數據", {storage: localStorage})
+     *
+     * Storage("數據", {value: 123, error: false})
+     * Storage("數據")
+     *
+     * Storage("數據", {value: 123, type: localStorage})
+     * Storage("數據", {type: localStorage})
      */
-    Storage(key, {storage=sessionStorage, value=null, error=undefined}={}) {
+    Storage(key, {type=sessionStorage, value=null, error=undefined}={}) {
         let data;
         return value != null
-            ? this.formula[this.formula.Type(value)](storage, key, value)
-            : (data = storage.getItem(key), data != undefined ? this.formula[this.formula.Type(JSON.parse(data))](storage, data) : error);
+            ? this.StorageMatch[this.StorageMatch.Type(value)](type, key, value)
+            : (data = type.getItem(key), data != undefined ? this.StorageMatch[this.StorageMatch.Type(JSON.parse(data))](type, data) : error);
     }
 
     /* ========== 油猴的 API ========== */
@@ -503,32 +505,33 @@ class Syntax {
      * // @grant GM_listValues
      * // @grant GM_deleteValue
      * 
-     * @param {string} operate - 操作類型 ("del", "all", "set", "get", "sjs", "gjs")
+     * @param {string} operate - 操作類型 ("s", "g", "sj", "gj", "de", "al")
      * @param {string} key     - 操作數據索引Key
-     * @param {*} orig         - 原始的回傳值
+     * @param {*} value        - 要保存的值
      * @returns {data}         - 獲取的數據值
      *
      * @example
      * 數據A = store("get", "資料A")
      * store("sjs", "資料B", "數據B")
      */
-    store(operat, key=null, value=null) {
-        const verify = val => (val !== void 0 ? val : null);
-        return {
-            del: (key) => GM_deleteValue(key),
-            all: () => verify(GM_listValues()),
-            set: (key, value) => GM_setValue(key, value),
-            get: (key, defaultValue) => verify(GM_getValue(key, defaultValue)),
-            sjs: (key, value) => GM_setValue(key, JSON.stringify(value, null, 4)),
-            gjs: (key, defaultValue) => JSON.parse(verify(GM_getValue(key, defaultValue)))
-        }[operat](key, value);
+    store(operat, key=null, value=null, error=null) {
+        const storeMatch = {
+            verify: val => val !== void 0 ? val : error,
+            de: key => GM_deleteValue(key),
+            al: () => storeMatch.verify(GM_listValues()),
+            s: (key, value) => GM_setValue(key, value),
+            g: (key) => storeMatch.verify(GM_getValue(key)),
+            sj: (key, value) => GM_setValue(key, JSON.stringify(value, null, 4)),
+            gj: (key) => JSON.parse(storeMatch.verify(GM_getValue(key)))
+        }
+        return storeMatch[operat](key, value);
     }
 
     /**
-     * { 監聽保存值的變化 }
+     ** { 監聽保存值的變化 }
      * 
      * // @grant GM_addValueChangeListener
-     * @param {array} object - 一個可遍歷的, 標籤對象物件
+     * @param {array} object    - 一個可遍歷的, 標籤對象物件
      * @param {object} callback - 回條函數
      * 
      * @example
