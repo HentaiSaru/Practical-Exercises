7bdad647baa0fc14f9254f850d35acf3f95f1ba5
TenshinoOtoKafu <s3459897@gmail.com>
2024-05-21 11:38:29 +0800

    測試清除

--- JavaScript/_Develop_/__Dev_Test__.js
+++
@@ -1,7 +0,0 @@
-const date = new Date();
-console.log(date.getFullYear()); // 年
-console.log(date.getMonth() + 1); // 月份 (從 0 開始，所以要加 1)
-console.log(date.getDate()); // 日
-console.log(date.getHours()); // 時
-console.log(date.getMinutes()); // 分
-console.log(date.getSeconds()); // 秒
