54e3a32ac9214a02ed9f5da607edb1e7afe4f23e
TenshinoOtoKafu <s3459897@gmail.com>
2024-06-24 07:22:17 +0800

    API 更新

--- JavaScript/Beta/ColaMangaEnhance.js
+++ JavaScript/Beta/ColaMangaEnhance.js
@@ -23,32 +23,30 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.5.2/jscolor.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js
-// @require      https://update.greasyfork.org/scripts/487608/1341419/GrammarSimplified.js
+// @require      https://update.greasyfork.org/scripts/487608/1382007/ClassSyntax_min.js
 // ==/UserScript==
 
 (function () {
     /**
      * 選單設置
-     * 
+     *
      * 背景色
      * 圖片基本寬度
      * 圖片最大寬度
-     * 
+     *
      * 開關功能
-     * 
+     *
      * 隱藏廣告
      * 快捷翻頁
      * 自動翻頁 => mode: 1 = 快速, 2 = 普通, 3 = 緩慢, 4 = 無盡 
      * 自動滾動 => 速度設置 / 換頁繼續滾動
-     * 
+     *
      * 請求 document.querySelector(".all_data_list") 主頁數據
      * 製做一個 iframe 或其他 將主頁的選擇漫畫列表複製到菜單中
-     * 
+     *
      * 返回目錄按鈕 返回首頁按鈕
      * 點選模態框關閉 並自動保存 (先隱藏 隔 1 秒刪除, 製作效果, 注意避免重複創建)
      * 模態需要設置特別的標籤 , 要避免被廣告阻擋函數的樣式擋住
-     * 
-     * 編譯 ColaMangaEnhance / 2 -> R:/U_Compiler / 1
      */
 
     /* 
@@ -56,12 +54,11 @@
         (0 = 不使用 | 1 = 使用 | mode = 有些有不同模式 2..3..n)
     */
     const Config = {
-        BlockAd: 1, // 阻擋廣告點擊
         BGColor: 1, // 背景換色 [目前還沒有自訂]
         RegisterHotkey: 3, // 快捷功能 mode: 1 = 翻頁, 2 = 翻頁+滾動, 3 翻頁+滾動+換頁繼續滾動
         AutoTurnPage: 4, // 自動換頁 mode: 1 = 快速, 2 = 普通, 3 = 緩慢, 4 = 無盡 (實驗中的酷東西)
     };
-    (new class Manga extends API {
+    (new class Manga extends Syntax {
         constructor() {
             super();
             this.ScrollPixels = 2; // 像素, 越高越快
@@ -74,68 +71,33 @@
             this.Up_scroll = this.Down_scroll = false; // 向上滾動, 向下滾動
             this.Observer_Next = null; // 下一頁觀察器
 
-            this.Origin = location.origin;
-            this.CurrentPage = document.URL;
-            this.IsMainPage = (window.self === window.parent);
-            this.RecordName = location.pathname.split("/")[1]; // 獲取漫畫標示符
-            this.RecordURL = this.Storage(localStorage, this.RecordName) || this.CurrentPage;
-
-            /* 獲取驅動訊息 (不要直接調用 前面有 _ 的) */
-            this.Device = {
-                sY: ()=> window.scrollY,
-                sX: ()=> window.scrollX,
-                Width: ()=> window.innerWidth,
-                Height: ()=> window.innerHeight,
-                Agent: ()=> navigator.userAgent,
-                _Type: undefined,
-                Type: function() {
-                    if (this._Type) {
-                        return this._Type;
-                    } else if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(this.Agent()) || this.Width() < 768) {
-                        this._Type = "Mobile";
-                    } else {
-                        this._Type = "Desktop";
-                    }
-                    return this._Type;
-                }
-            };
+            this.IsMainPage = (window.self === window.parent); // 判斷是非 iframe
+            this.BlockListener = new Set([
+                "pointerup", "pointerdown", "dState", "touchstart", "unhandledrejection"
+            ]);
 
             /* 取得數據 */
             this.Get_Data = async (callback) => {
-                if (this.Device.Type() == "Mobile") { // 手機版進行複寫
-                    this.WaitMap = (selectors, timeout, callback, object) => {
-                        let timer, elements;
-                        const Interval = setInterval(() => {
-                            elements = selectors.map(selector => document.querySelector(selector))
-                            if (elements.every(element => {return element !== null && typeof "undefined"})) {
-                                clearInterval(Interval);
-                                clearTimeout(timer);
-                                callback(elements);
-                            }
-                        }, 100);
-                        timer = setTimeout(() => {clearInterval(Interval)}, (1000 * timeout));
-                    }
-                }
-                this.WaitMap(["body", "div.mh_readtitle", "div.mh_headpager", "div.mh_readend", "#mangalist"], 20, element => {
+                this.WaitMap(["body", "div.mh_readtitle", "div.mh_headpager", "div.mh_readend", "#mangalist"], element => {
                     const [Body, Title, HeadPager, Readend, Manga] = element;
                     this.Body = Body;
 
-                    const HomeLink = this.$$("a", true, Title);
+                    const HomeLink = this.$$("a", {all: true, root: Title});
                     this.ContentsPage = HomeLink[0].href; // 目錄連結
                     this.HomePage = HomeLink[1].href; // 首頁連結
 
                     try {
-                        const PageLink = this.$$("ul a", true, Readend);
+                        const PageLink = this.$$("ul a", {all: true, root: Readend});
                         this.PreviousPage = PageLink[0].href;
                         this.NextPage = PageLink[2].href;
                     } catch {
-                        const PageLink = this.$$("a.mh_btn:not(.mh_bgcolor)", true, HeadPager);
+                        const PageLink = this.$$("a.mh_btn:not(.mh_bgcolor)", {all: true, root: HeadPager});
                         this.PreviousPage = PageLink[0].href;
                         this.NextPage = PageLink[1].href;
                     }
 
                     this.MangaList = Manga; // 漫畫列表
-                    this.BottomStrip = this.$$("a", false, Readend); // 以閱讀完畢的那條, 看到他跳轉
+                    this.BottomStrip = this.$$("a", {root: Readend}); // 以閱讀完畢的那條, 看到他跳轉
 
                     if([
                         this.Body,
@@ -145,17 +107,16 @@
                         this.NextPage,
                         this.MangaList,
                         this.BottomStrip
-                    ].every(Check => Check))
-                    {callback(true)}
-                    else {callback(false)}
-                }, document);
+                    ].every(Check => Check)) callback(true);
+                    else callback(false);
+                }, {timeout: 10, timeoutResult: true, object: document});
             };
 
             /* 存取會話數據 */
             this.storage = (key, value=null) => {
                 return value != null
-                ? this.Storage(sessionStorage, key, value) 
-                : this.Storage(sessionStorage, key);
+                ? this.Storage(key, {value: value}) 
+                : this.Storage(key);
             }
 
             /* 檢測跳轉連結 */
@@ -164,13 +125,13 @@
             };
 
             /* 檢測到頂 */
-            this.TopDetected = this.Throttle_discard(()=>{
+            this.TopDetected = this.Throttle(()=>{
                 this.Up_scroll = this.Device.sY() == 0 ? (this.storage("scroll", false), false) : true;
             }, 1000);
             /* 檢測到底 */
-            this.BottomDetected = this.Throttle_discard(()=>{
+            this.BottomDetected = this.Throttle(()=>{
                 this.Down_scroll =
-                this.Device.sY() + this.Device.Height() >= document.documentElement.scrollHeight ? (this.storage("scroll", false), false) : true;
+                this.Device.sY() + this.Device.iH() >= document.documentElement.scrollHeight ? (this.storage("scroll", false), false) : true;
             }, 1000);
 
             /* 自動滾動 (邏輯修改) */
@@ -199,7 +160,7 @@
 
             /* 獲取樣式 */
             this.Get_Style = () => {
-                const Style = this.store("get", "Style") ||
+                const Style = this.Store("g", "Style") ||
                     [{
                         "BG_Color": "#595959",
                         "Img_Bw": "auto",
@@ -211,29 +172,56 @@
             this.ImgStyle = this.Get_Style();
         }
 
-        /* 阻擋廣告 */
+        /* 阻擋廣告 (目前無效) */
         async BlockAds() {
+            const OriginListener = EventTarget.prototype.addEventListener, Block = this.BlockListener;
+            const EventListeners = new Map();
+
+            EventTarget.prototype.addEventListener = function(type, listener, options) {
+                if (Block.has(type)) return;
+                if (!EventListeners.has(this)) EventListeners.set(this, []);
+                EventListeners.get(this).push({ type, listener, options });
+                OriginListener.call(this, type, listener, options);
+            };
+
+            function removeBlockedListeners() {
+                EventListeners.forEach((listeners, element) => {
+                    listeners.forEach(({ type, listener }) => {
+                        if (Block.has(type)) {
+                            element.removeEventListener(type, listener);
+                        }
+                    });
+                });
+            }
+
+            this.AddStyle(`
+                html {pointer-events: none !important;}
+                .mh_wrap, span.mh_btn:not(.contact), #Iframe-Comics {pointer-events: auto;}
+            `, "Inject-Blocking-Ads");
+
             // 雖然性能開銷比較高, 但比較不會跳一堆錯誤訊息
             let iframe;
-            this.AdCleanup = setInterval(() => {iframe = this.$$("iframe:not(#Iframe-Comics)"); iframe && iframe.remove()}, 600);
-            if (this.Device.Type() == "Desktop") {
-                this.AddStyle(`
-                    html {pointer-events: none !important;}
-                    .mh_wrap, span.mh_btn:not(.contact), #Iframe-Comics {pointer-events: auto;}
-                `, "Inject-Blocking-Ads");
-
-            } else if (this.Device.Type() == "Mobile") {
-                const OriginListener = EventTarget.prototype.addEventListener;
-                EventTarget.prototype.addEventListener = function(type, listener, options) {
-                    if (type === "pointerup") {return}
-                    OriginListener.call(this, type, listener, options);
-                };
-            }
+            this.AdCleanup = setInterval(() => {
+                console.log(document.documentElement.scrollHeight);
+                iframe = this.$$("iframe:not(#Iframe-Comics)"); iframe && iframe.remove();
+                removeBlockedListeners();
+            }, 500);
         }
 
         /* 背景樣式 */
         async BackgroundStyle() {
             this.Body.style.backgroundColor=this.ImgStyle.BG_Color;
+        }
+
+        /* 自動重新加載死圖 */
+        async AutoReload() {
+            let click = new MouseEvent("click", { bubbles: true, cancelable: true });
+            const observer = new IntersectionObserver(observed => {
+                observed.forEach(entry => {
+                    if (entry.isIntersecting) {entry.target.dispatchEvent(click)}
+                });
+            }, { threshold: .3 });
+            this.$$("span.mh_btn:not(.contact)", {all: true, root: this.MangaList}).forEach(b => { observer.observe(b) });
         }
 
         /* 圖片樣式 */
@@ -248,17 +236,6 @@
                 `, "Inject-Image-Style");
             }
             setTimeout(()=>{this.AutoReload()}, this.WaitPicture);
-        }
-
-        /* 自動重新載入 */
-        async AutoReload() {
-            let click = new MouseEvent("click", { bubbles: true, cancelable: true });
-            const observer = new IntersectionObserver(observed => {
-                observed.forEach(entry => { 
-                    if (entry.isIntersecting) {entry.target.dispatchEvent(click)}
-                });
-            }, { threshold: .3 });
-            this.$$("span.mh_btn:not(.contact)", true, this.MangaList).forEach(b => { observer.observe(b) });
         }
 
         /* 快捷切換上下頁 和 自動滾動 */
@@ -309,7 +286,7 @@
                 }, { capture: true });
             } else if (this.Device.Type() == "Mobile") {
 
-                const sidelineX = .35 * this.Device.Width(), sidelineY = (this.Device.Height() / 4) * .2;
+                const sidelineX = .35 * this.Device.iW(), sidelineY = (this.Device.iH() / 4) * .2;
                 let startX, startY, moveX, moveY;
 
                 this.Listen(window, "touchstart", event => {
@@ -366,7 +343,7 @@
 
             if (mode != 4) {
                 setTimeout(()=> {
-                    img = self.$$("img", true, self.MangaList);
+                    img = self.$$("img", {all: true, root: self.MangaList});
                     self.Observer_Next.observe(object);
                 }, self.WaitPicture);
             }
@@ -398,7 +375,7 @@
             }, { threshold: .1 });
 
             setTimeout(()=> {// 想辦法讓監聽對象變成動態調整
-                img = this.$$("img", true, this.MangaList);
+                img = this.$$("img", {all: true, root: this.MangaList});
                 this.Observer_Next.observe(this.ObserveValue(img));
             }, this.WaitPicture);
 
@@ -417,10 +394,9 @@
                 })
             }
 
-            this.Buffer.appendChild(iframe); // 添加緩衝
             async function TurnPage() {
                 requestAnimationFrame(() => { // 載入 iframe
-                    document.body.appendChild(self.Buffer); Waitload();
+                    document.body.appendChild(iframe); Waitload();
                 });
             
                 // 等待 iframe 載入完成
@@ -445,7 +421,7 @@
                                 }
                             });
                         }, { threshold: .1 });
-                        UrlUpdate.observe(self.$$("#mangalist img", false, Content));
+                        UrlUpdate.observe(self.$$("#mangalist img", {root: Content}));
                     };
                     iframe.onerror = () => {
                         setTimeout(()=> {iframe.src = self.NextPage; Waitload();}, 1500);
@@ -465,17 +441,17 @@
 
         /* 功能注入 */
         async Injection() {
+            this.BlockAds();
             try {
-                Config.BlockAd > 0 && this.BlockAds();
                 this.Get_Data(state=> {
                     if (state) {
                         Config.BGColor > 0 && this.BackgroundStyle();
                         this.PictureStyle();
                         Config.RegisterHotkey > 0 && this.Hotkey_Switch(Config.RegisterHotkey);
                         Config.AutoTurnPage > 0 && this.Automatic_Next(Config.AutoTurnPage);
-                    }
+                    } else this.Log(null, "Error");
                 });
-            } catch (error) { this.log(null, error) }
+            } catch (error) { this.Log(null, error) }
         }
     }).Injection();
 })();
