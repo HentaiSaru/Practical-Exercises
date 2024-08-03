0fc7277339e8915816223fa0e27436d2bc6eee99
TenshinoOtoKafu <s3459897@gmail.com>
2024-08-03 11:53:54 +0800

    調整處理

--- JavaScript/_Develop_/Kemer下載器_Json處理.mjs
+++ JavaScript/_Develop_/Kemer下載器_Json處理.mjs
@@ -1,13 +1,31 @@
-import { readFile } from "fs";
+import open from "fs";
 
-const json_path = ""
+function Out(Path, Data) {
+    open.writeFile(Path, Data, err => {
+        err ? console.log(`輸出失敗: ${err}`) : console.log("輸出成功");
+    });
+}
 
-readFile(json_path, "utf-8", (err, data)=> {
-    for (const value of Object.values(JSON.parse(data))) {
-        try {
-            for (const [name, link] of Object.entries(value["下載連結"])) {
-                console.log(`${link}?f=${name}`);
+function Read(Path) {
+    return new Promise((resolve, reject) => {
+        let Cache = "";
+        open.readFile(Path, "utf-8", (err, data) => {
+            if (err) return reject(err);
+
+            for (const value of Object.values(JSON.parse(data))) {
+                try {
+                    for (const [name, link] of Object.entries(value["下載連結"])) {
+                        Cache += `${link}?f=${name}\n`;
+                    }
+                } catch (error) {}
             }
-        } catch {}
-    }
-});
+
+            if (Cache.endsWith('\n')) Cache = Cache.slice(0, -1); // 如果最後一行是 \n 就排除掉這行
+            resolve(Cache);
+        });
+    });
+};
+
+Read("").then(read=> {
+    Out("R:/Kemer.txt", read);
+})
