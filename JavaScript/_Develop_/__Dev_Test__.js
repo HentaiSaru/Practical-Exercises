804e8b84e3e9f0de5baa8c16c2aced84f856dae0
TenshinoOtoKafu <s3459897@gmail.com>
2024-09-14 19:13:45 +0800

    格式調整

--- JavaScript/_Develop_/__Dev_Test__.js
+++ JavaScript/_Develop_/__Dev_Test__.js
@@ -16,32 +16,32 @@ const user = [
 const Type = (object) => Object.prototype.toString.call(object).slice(8, -1);
 
 const Print = { // Log()
-    log: msg=> console.log(msg),
-    warn: msg=> console.warn(msg),
-    table: msg=> console.table(msg),
-    trace: msg=> console.trace(msg),
-    error: msg=> console.error(msg),
-    count: msg=> console.count(msg),
+    log: msg => console.log(msg),
+    warn: msg => console.warn(msg),
+    table: msg => console.table(msg),
+    trace: msg => console.trace(msg),
+    error: msg => console.error(msg),
+    count: msg => console.count(msg),
 };
 
-async function Log(group=null, msg="print", {
-    dev=true,
-    type="log",
-    each=false,
-    collapsed=true
-}={}) {
+async function Log(group = null, msg = "print", {
+    dev = true,
+    type = "log",
+    each = false,
+    collapsed = true
+} = {}) {
     if (!dev) return;
 
     let PrintCall, Cache;
     if (each) {
         PrintCall = {
-            Array: (msg)=> {
+            Array: (msg) => {
                 for (Cache of msg) {
-                    
+
                 }
             },
-            Object: (msg)=> {
-                
+            Object: (msg) => {
+
             },
         };
     } else PrintCall = Print;
@@ -58,7 +58,7 @@ async function Log(group=null, msg="print", {
 //Log(null, {"頭": document.head, "身體": document.body, "全": document.documentElement});
 
 let test = ["list1", "list2", "list3"];
-let test1 = {"object1": "物件1", "object2": "物件2", "object3": "物件3"};
+let test1 = { "object1": "物件1", "object2": "物件2", "object3": "物件3" };
 "Array"
 "Object"
 
