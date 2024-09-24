865f06b87b66b1591a5e36960e46a517f4123f0c
TenshinoOtoKafu <s3459897@gmail.com>
2024-09-24 10:47:05 +0800

    排版格式化

--- JavaScript/Beta/TwitchReceiveDrops.js
+++ JavaScript/Beta/TwitchReceiveDrops.js
@@ -49,13 +49,13 @@
     class Detection {
         constructor() {
             /* 保存數據 */
-            this.Storage = (key, value=null) => {
+            this.Storage = (key, value = null) => {
                 let data,
-                Formula = {
-                    Type: (parse) => Object.prototype.toString.call(parse).slice(8, -1),
-                    Number: (parse) => parse ? Number(parse) : (sessionStorage.setItem(key, JSON.stringify(value)), !0),
-                    Array: (parse) => parse ? JSON.parse(parse) : (sessionStorage.setItem(key, JSON.stringify(value)), !0),
-                };
+                    Formula = {
+                        Type: (parse) => Object.prototype.toString.call(parse).slice(8, -1),
+                        Number: (parse) => parse ? Number(parse) : (sessionStorage.setItem(key, JSON.stringify(value)), !0),
+                        Array: (parse) => parse ? JSON.parse(parse) : (sessionStorage.setItem(key, JSON.stringify(value)), !0),
+                    };
                 return null != value
                     ? Formula[Formula.Type(value)]()
                     : !!(data = sessionStorage.getItem(key)) && Formula[Formula.Type(JSON.parse(data))](data);
@@ -111,9 +111,9 @@
 
             /* 展示進度於 */
             this.ShowProgress = () => {
-                (new MutationObserver(()=> {
+                (new MutationObserver(() => {
                     document.title != this.ProgressValue && (document.title = this.ProgressValue);
-                })).observe(document.querySelector("title"), {childList: !0, subtree: !1});
+                })).observe(document.querySelector("title"), { childList: !0, subtree: !1 });
                 document.title = this.ProgressValue; // 觸發一次轉換
             };
 
@@ -125,9 +125,11 @@
                         if (interval <= 0) location.reload();
                     }, 1e3);
                 } else { // 不展示
-                    setTimeout(()=> {location.reload()}, interval);
+                    setTimeout(() => { location.reload() }, interval);
                 }
             };
+
+            this.ProgressValue = "";
 
             /* 設置數據 */
             this.config = Object.assign(Config, {
@@ -136,8 +138,6 @@
                 ProgressBar: "p.mLvNZ span", // 掉寶進度數據
                 ActivityTime: "span.jSkguG", // 掉寶活動的日期
             });
-
-            this.ProgressValue = "";
         }
 
         /* 主要運行 */
@@ -151,18 +151,18 @@
             const Display = Self.UpdateDisplay;
             const Interval = Self.UpdateInterval;
 
-            const observer = new MutationObserver(Throttle(()=> {
+            const observer = new MutationObserver(Throttle(() => {
                 if (Deal) { // 這邊寫這麼複雜是為了處理, (1: 只有一個, 2: 存在兩個以上, 3: 存在兩個以上但有些過期)
                     const All_Data = document.querySelectorAll(Self.AllProgress);
 
                     if (All_Data && All_Data.length > 0) {
                         Deal = !1;
 
-                        All_Data.forEach((data, index)=> {
+                        All_Data.forEach((data, index) => {
                             Detec.TimeComparison(data, data.querySelector(Self.ActivityTime).textContent,
-                                NotExpired=> { // 標題顯示進度, 和重啟是分開的, 所以無論如何都會獲取當前進度
+                                NotExpired => { // 標題顯示進度, 和重啟是分開的, 所以無論如何都會獲取當前進度
                                     // 分別取得各自的進度
-                                    Progress_Info[index] = [...NotExpired.querySelectorAll(Self.ProgressBar)].map(progress=> +progress.textContent);
+                                    Progress_Info[index] = [...NotExpired.querySelectorAll(Self.ProgressBar)].map(progress => +progress.textContent);
                                 }
                             )
                         });
@@ -190,7 +190,7 @@
                     Self.RestartLive = !1;
 
                     const time = new Date(), // 格式為 = 進度值, 時間戳
-                    [ProgressRecord, Timestamp] = Detec.Storage("Record") || [progress, Detec.GetTime(time)], conversion = ~~ ((time - new Date(Timestamp)) / (1e3 * 60)); // 捨棄小數後取整, ~~ 最多限制 32 位整數
+                        [ProgressRecord, Timestamp] = Detec.Storage("Record") ?? [progress, Detec.GetTime(time)], conversion = ~~((time - new Date(Timestamp)) / (1e3 * 60)); // 捨棄小數後取整, ~~ 最多限制 32 位整數
 
                     if (conversion >= Self.JudgmentInterval && progress == ProgressRecord) { // 時間大於檢測間隔, 且標題與進度值相同, 代表需要重啟
                         Restart.Ran(PI); // 傳遞當前最高進度, 進行直播重啟
@@ -202,12 +202,12 @@
                 };
 
                 // 領取按鈕
-                document.querySelectorAll(Self.DropsButton).forEach(draw => {draw.click()});
+                document.querySelectorAll(Self.DropsButton).forEach(draw => { draw.click() });
 
                 if (document.querySelector(Self.EndLine)) {
                     observer.disconnect(); // 斷開觀察
- 
-                    const count = Detec.Storage("NoProgressCount") || 0;
+
+                    const count = Detec.Storage("NoProgressCount") ?? 0;
                     if (progress) {
                         Detec.Storage("NoProgressCount", 0);
                     } else if (count > 2) {
@@ -217,13 +217,13 @@
                             window.close();
                         }
                     } else {
-                        Detec.Storage("NoProgressCount", count+1);
+                        Detec.Storage("NoProgressCount", count + 1);
                     }
                 };
             }, 300));
 
             // 因為是後台運行, 使用 requestAnimationFrame 查找會不太穩定, 只能使用 MutationObserver
-            observer.observe(document, {subtree: !0, childList: !0, characterData: !0});
+            observer.observe(document, { subtree: !0, childList: !0, characterData: !0 });
             Self.TryStayActive && StayActive(document);
             Detec.PageRefresh(Display, Display ? Interval : Interval * 1e3); // 呼叫頁面刷新
         }
@@ -235,20 +235,20 @@
             /* 簡易的 等待元素 */
             this.WaitElem = async (Newindow, selector, found) => {
                 let element;
-                const observer = new MutationObserver(Throttle(()=> {
+                const observer = new MutationObserver(Throttle(() => {
                     element = Newindow.document.querySelector(selector);
                     element && (observer.disconnect(), found(element));
                 }, 200));
-                observer.observe(Newindow.document, {subtree: !0, childList: !0, characterData: !0});
-            }
+                observer.observe(Newindow.document, { subtree: !0, childList: !0, characterData: !0 });
+            };
 
             /* 重啟直播的靜音(持續執行 15 秒) */
             this.LiveMute = async Newindow => {
                 this.WaitElem(Newindow, "video", video => {
-                    const SilentInterval = setInterval(() => {video.muted = !0}, 5e2);
-                    setTimeout(()=> {clearInterval(SilentInterval)}, 1.5e4);
+                    const SilentInterval = setInterval(() => { video.muted = !0 }, 5e2);
+                    setTimeout(() => { clearInterval(SilentInterval) }, 1.5e4);
                 })
-            }
+            };
 
             /* 直播自動最低畫質 */
             this.LiveLowQuality = async Newindow => {
@@ -258,11 +258,11 @@
                         Quality.click(); // 點擊畫質設定
                         this.WaitElem(Newindow, "[data-a-target='player-settings-menu']", Settings => {
                             Settings.lastElementChild.click(); // 選擇最低畫質
-                            setTimeout(() => {Menu.click()}, 800); // 等待一下關閉菜單
+                            setTimeout(() => { Menu.click() }, 800); // 等待一下關閉菜單
                         })
                     })
                 })
-            }
+            };
 
             this.config = Object.assign(Config, {
                 TagType: "span", // 頻道 Tag 標籤
@@ -293,7 +293,7 @@
 
                 FindLive(0);
                 async function FindLive(index) { // 持續找到有在直播的頻道
-                    if ((OpenLink.length-1) < index) {
+                    if ((OpenLink.length - 1) < index) {
                         return !1;
                     }
 
@@ -304,13 +304,13 @@
                         DirectorySearch(NewWindow);
                     } else {
                         let Offline, Nowlive;
-                        const observer = new MutationObserver(Throttle(()=> {
+                        const observer = new MutationObserver(Throttle(() => {
                             Offline = NewWindow.document.querySelector(Self.OfflineTag);
                             Nowlive = NewWindow.document.querySelector(Self.ViewersTag);
 
                             if (Offline) {
                                 observer.disconnect();
-                                FindLive(index+1);
+                                FindLive(index + 1);
 
                             } else if (Nowlive) {
                                 observer.disconnect();
@@ -322,7 +322,7 @@
                         }, 300));
 
                         NewWindow.onload = () => {
-                            observer.observe(NewWindow.document, {subtree: !0, childList: !0, characterData: !0});
+                            observer.observe(NewWindow.document, { subtree: !0, childList: !0, characterData: !0 });
                         }
                     }
                 }
@@ -330,7 +330,7 @@
 
             // 目錄頁面的查找邏輯
             async function DirectorySearch(NewWindow) {
-                const observer = new MutationObserver(Throttle(()=> {
+                const observer = new MutationObserver(Throttle(() => {
                     article = NewWindow.document.getElementsByTagName(Self.Article);
                     if (article.length > 10) { // 找到大於 10 個頻道
                         observer.disconnect();
@@ -350,10 +350,10 @@
                             function Language(lang) {
                                 const Word = {
                                     Traditional: {},
-                                    Simplified: {搜尋失敗: "搜索失败", 找不到啟用掉落的頻道: "找不到启用掉落的频道"},
-                                    Korea: {搜尋失敗: "검색 실패", 找不到啟用掉落的頻道: "드롭이 활성화된 채널을 찾을 수 없습니다"},
-                                    Japan: {搜尋失敗: "検索失敗", 找不到啟用掉落的頻道: "ドロップが有効なチャンネルが見つかりません"},
-                                    English: {搜尋失敗: "Search failed", 找不到啟用掉落的頻道: "Can't find a channel with drops enabled"},
+                                    Simplified: { 搜尋失敗: "搜索失败", 找不到啟用掉落的頻道: "找不到启用掉落的频道" },
+                                    Korea: { 搜尋失敗: "검색 실패", 找不到啟用掉落的頻道: "드롭이 활성화된 채널을 찾을 수 없습니다" },
+                                    Japan: { 搜尋失敗: "検索失敗", 找不到啟用掉落的頻道: "ドロップが有効なチャンネルが見つかりません" },
+                                    English: { 搜尋失敗: "Search failed", 找不到啟用掉落的頻道: "Can't find a channel with drops enabled" },
                                 }, Match = {
                                     ko: Word.Korea,
                                     ja: Word.Japan,
@@ -364,7 +364,7 @@
                                     "zh-HK": Word.Traditional,
                                     "zh-MO": Word.Traditional,
                                 },
-                                ML = Match[lang] ?? Match["en-US"];
+                                    ML = Match[lang] ?? Match["en-US"];
 
                                 return {
                                     Transl: (Str) => ML[Str] ?? Str,
@@ -381,7 +381,7 @@
                 }, 300));
 
                 NewWindow.onload = () => {
-                    observer.observe(NewWindow.document, {subtree: !0, childList: !0, characterData: !0});
+                    observer.observe(NewWindow.document, { subtree: !0, childList: !0, characterData: !0 });
                 }
             }
         }
