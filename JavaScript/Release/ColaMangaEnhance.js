b828d8438b7ae67f08304fa26112f07e2f5dcd7c
TenshinoOtoKafu <s3459897@gmail.com>
2024-09-27 05:21:30 +0800

    版本更新

--- JavaScript/Release/ColaMangaEnhance.js
+++ JavaScript/Release/ColaMangaEnhance.js
@@ -3,7 +3,7 @@
 // @name:zh-TW   ColaManga 瀏覽增強
 // @name:zh-CN   ColaManga 浏览增强
 // @name:en      ColaManga Browsing Enhancement
-// @version      0.0.11-Beta1
+// @version      0.0.11-Beta2
 // @author       Canaan HS
 // @description       隱藏廣告內容，提昇瀏覽體驗。自訂背景顏色，圖片大小調整。當圖片載入失敗時，自動重新載入圖片。提供熱鍵功能：[← 上一頁]、[下一頁 →]、[↑ 自動上滾動]、[↓ 自動下滾動]。當用戶滾動到頁面底部時，自動跳轉到下一頁。
 // @description:zh-TW 隱藏廣告內容，提昇瀏覽體驗。自訂背景顏色，圖片大小調整。當圖片載入失敗時，自動重新載入圖片。提供熱鍵功能：[← 上一頁]、[下一頁 →]、[↑ 自動上滾動]、[↓ 自動下滾動]。當用戶滾動到頁面底部時，自動跳轉到下一頁。
@@ -24,14 +24,14 @@
 // ==/UserScript==
 
 (async () => {
-    /* 
+    /*
         設置是否使用該功能, 沒有設定參數, 這只是臨時的寫法, 之後會刪除掉
         (0 = 不使用 | 1 = 使用 | mode = 有些有不同模式 2..3..n)
     */
     const Config = {
         BGColor: 1, // 背景換色 [目前還沒有自訂], 改代碼可搜索 #595959, 並修改該字串
-        RegisterHotkey: 3, // 快捷功能 mode: 1 = 翻頁, 2 = 翻頁+滾動, 3 翻頁+滾動+換頁繼續滾動
-        AutoTurnPage: 4, // 自動換頁 mode: 1 = 快速, 2 = 普通, 3 = 緩慢, 4 = 無盡 (實驗中的酷東西)
+        RegisterHotkey: 3, // 快捷功能 mode: 1 = 翻頁 | 2 = 翻頁+滾動 | 3 翻頁+滾動+換頁繼續滾動
+        AutoTurnPage: 5, // 自動換頁 mode: 1 = 快速 | 2 = 普通 | 3 = 緩慢 | 4 = 一般無盡 | 5 = 優化無盡
     };
     new class Manga extends Syntax {
         constructor() {
@@ -46,10 +46,19 @@
             this.Up_scroll = this.Down_scroll = false;
             this.Observer_Next = null;
             this.IsMainPage = window.self === window.parent;
-            this.BlockListener = new Set([ "pointerup", "pointerdown", "dState", "touchstart", "unhandledrejection" ]);
+            this.BlockListener = new Set(["pointerup", "pointerdown", "dState", "touchstart", "unhandledrejection"]);
+            this.Id = {
+                Title: "CME_Title",
+                Iframe: "CME_Iframe",
+                Block: "CME_Block-Ads",
+                Menu: "CME_Menu-Style",
+                Image: "CME_Image-Style",
+                Scroll: "CME_Scroll-Hidden",
+                ChildS: "CME_Child-Scroll-Hidden"
+            };
             this.Get_Data = async callback => {
-                this.WaitMap([ "body", "div.mh_readtitle", "div.mh_headpager", "div.mh_readend", "#mangalist" ], element => {
-                    const [ Body, Title, HeadPager, Readend, Manga ] = element;
+                this.WaitMap(["body", "div.mh_readtitle", "div.mh_headpager", "div.mh_readend", "#mangalist"], element => {
+                    const [Body, Title, HeadPager, Readend, Manga] = element;
                     this.Body = Body;
                     const HomeLink = this.$$("a", {
                         all: true,
@@ -76,12 +85,32 @@
                     this.BottomStrip = this.$$("a", {
                         root: Readend
                     });
-                    if ([ this.Body, this.ContentsPage, this.HomePage, this.PreviousPage, this.NextPage, this.MangaList, this.BottomStrip ].every(Check => Check)) callback(true); else callback(false);
+                    if ([this.Body, this.ContentsPage, this.HomePage, this.PreviousPage, this.NextPage, this.MangaList, this.BottomStrip].every(Check => Check)) callback(true); else callback(false);
                 }, {
                     timeout: 10,
                     timeoutResult: true,
                     object: document
                 });
+            };
+            this.Get_Nodes = Root => {
+                const nodes = [];
+                const IdWhiteList = new Set(Object.values(this.Id));
+                function Task(root) {
+                    const tree = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, {
+                        acceptNode: node => {
+                            if (IdWhiteList.has(node.id)) {
+                                return NodeFilter.FILTER_REJECT;
+                            }
+                            return NodeFilter.FILTER_ACCEPT;
+                        }
+                    });
+                    while (tree.nextNode()) {
+                        nodes.push(tree.currentNode);
+                    }
+                }
+                Task(Root.head);
+                Task(Root.body);
+                return nodes;
             };
             this.storage = (key, value = null) => {
                 return value != null ? this.Storage(key, {
@@ -89,12 +118,12 @@
                 }) : this.Storage(key);
             };
             this.TopDetected = this.Throttle(() => {
-                this.Up_scroll = this.Device.sY() == 0 ? (this.storage("scroll", false), 
-                false) : true;
+                this.Up_scroll = this.Device.sY() == 0 ? (this.storage("scroll", false),
+                    false) : true;
             }, 1e3);
             this.BottomDetected = this.Throttle(() => {
-                this.Down_scroll = this.Device.sY() + this.Device.iH() >= document.documentElement.scrollHeight ? (this.storage("scroll", false), 
-                false) : true;
+                this.Down_scroll = this.Device.sY() + this.Device.iH() >= document.documentElement.scrollHeight ? (this.storage("scroll", false),
+                    false) : true;
             }, 1e3);
             this.scroll = move => {
                 requestAnimationFrame(() => {
@@ -114,11 +143,11 @@
             this.ObserveObject = object => object[Math.max(object.length - 2, 0)];
             this.DetectionValue = object => this.VisibleObjects(object).length >= Math.floor(object.length * .5);
             this.Get_Style = () => {
-                const Style = this.Store("g", "Style") || [ {
+                const Style = this.Store("g", "Style") || [{
                     BG_Color: "#595959",
                     Img_Bw: "auto",
                     Img_Mw: "100%"
-                } ];
+                }];
                 return Style[0];
             };
             this.ImgStyle = this.Get_Style();
@@ -126,7 +155,7 @@
         async BlockAds() {
             const OriginListener = EventTarget.prototype.addEventListener, Block = this.BlockListener;
             const EventListeners = new Map();
-            EventTarget.prototype.addEventListener = function(type, listener, options) {
+            EventTarget.prototype.addEventListener = function (type, listener, options) {
                 if (Block.has(type)) return;
                 if (!EventListeners.has(this)) EventListeners.set(this, []);
                 EventListeners.get(this).push({
@@ -150,10 +179,10 @@
             }
             this.AddStyle(`
                 html {pointer-events: none !important;}
-                .mh_wrap, span.mh_btn:not(.contact), #Iframe-Comics {pointer-events: auto;}
-            `, "Inject-Blocking-Ads");
+                .mh_wrap, span.mh_btn:not(.contact), ${this.Id.Iframe} {pointer-events: auto;}
+            `, this.Id.Block);
             this.AdCleanup = setInterval(() => {
-                this.$$("iframe:not(#Iframe-Comics)")?.remove();
+                this.$$(`iframe:not(#${this.Id.Iframe})`)?.remove();
                 removeBlockedListeners();
             }, 500);
         }
@@ -194,7 +223,7 @@
                         width: ${this.ImgStyle.Img_Bw};
                         max-width: ${this.ImgStyle.Img_Mw};
                     }
-                `, "Inject-Image-Style");
+                `, this.Id.Image);
             }
             setTimeout(() => {
                 this.AutoReload();
@@ -272,7 +301,7 @@
             }
         }
         async AutoPageTurn(mode) {
-            let self = this, hold, object, img;
+            let self = this, hold, point, img;
             self.Observer_Next = new IntersectionObserver(observed => {
                 observed.forEach(entry => {
                     if (entry.isIntersecting && self.DetectionValue(img)) {
@@ -284,49 +313,55 @@
                 threshold: hold
             });
             switch (mode) {
-              case 2:
-                hold = .5;
-                object = self.$$("li:nth-child(3) a.read_page_link");
-                break;
+                case 2:
+                    hold = .5;
+                    point = self.$$("li:nth-child(3) a.read_page_link");
+                    break;
 
-              case 3:
-                hold = 1;
-                object = self.$$("div.endtip2.clear");
-                break;
+                case 3:
+                    hold = 1;
+                    point = self.$$("div.endtip2.clear");
+                    break;
 
-              case 4:
-                this.UnlimitedPageTurn();
-                break;
+                case 4:
+                case 5:
+                    this.UnlimitedPageTurn(mode == 5);
+                    break;
 
-              default:
-                hold = .1;
-                object = self.BottomStrip;
+                default:
+                    hold = .1;
+                    point = self.BottomStrip;
             }
-            if (mode != 4) {
+            if (point) {
                 setTimeout(() => {
                     img = self.$$("img", {
                         all: true,
                         root: self.MangaList
                     });
-                    self.Observer_Next.observe(object);
+                    self.Observer_Next.observe(point);
                 }, self.WaitPicture);
             }
         }
-        async UnlimitedPageTurn() {
+        async UnlimitedPageTurn(Optimized) {
             const self = this;
             const iframe = document.createElement("iframe");
-            iframe.id = "Iframe-Comics";
+            iframe.id = this.Id.Iframe;
             iframe.src = self.NextPage;
             this.AddStyle(`
-                .mh_wrap, .mh_readend, .mh_footpager, .fed-foot-info, #imgvalidation2022 {display: none;}
-                #Iframe-Comics {
+                .mh_wrap, .mh_readend, .mh_footpager,
+                .fed-foot-info, #imgvalidation2022 {display: none;}
+                body {
+                    margin: 0;
+                    padding: 0;
+                }
+                #${this.Id.Iframe} {
                     margin: 0;
                     padding: 0;
                     height: 110vh;
                     width: 100%;
                     border: none;
                 }
-            `, "scroll-hidden");
+            `, this.Id.Scroll);
             if (this.IsMainPage) {
                 document.documentElement.style.cssText = `
                     overflow: visible !important;
@@ -344,10 +379,10 @@
                         scrollbar-width: none !important;
                         -ms-overflow-style: none !important;
                     }
-                    html::-webkit-scrollbar { 
+                    html::-webkit-scrollbar {
                         display: none !important;
                     }
-                `, "child-scroll-hidden");
+                `, this.Id.ChildS);
                 let MainWindow = window;
                 this.Listen(window, "message", event => {
                     while (MainWindow.parent !== MainWindow) {
@@ -394,7 +429,7 @@
                 }, self.WaitPicture);
             }
             async function TurnPage() {
-                let URL, Content, StylelRules = self.$$("#scroll-hidden").sheet.cssRules;
+                let URL, Content, StylelRules = self.$$(`#${self.Id.Scroll}`).sheet.cssRules;
                 if (self.FinalPage(self.NextPage)) {
                     StylelRules[0].style.display = "block";
                     return;
@@ -405,25 +440,43 @@
                     iframe.onload = () => {
                         URL = iframe.contentWindow.location.href;
                         URL != self.NextPage && (iframe.src = self.NextPage, Waitload());
-                        self.Log("無盡翻頁", self.NextPage);
                         Content = iframe.contentWindow.document;
                         Content.body.style.overflow = "hidden";
+                        const TopImg = self.$$("#mangalist img", {
+                            root: Content
+                        });
                         setInterval(() => {
-                            StylelRules[1].style.height = `${Content.body.scrollHeight}px`;
+                            StylelRules[2].style.height = `${Content.body.scrollHeight}px`;
                         }, 1500);
                         const UrlUpdate = new IntersectionObserver(observed => {
                             observed.forEach(entry => {
                                 if (entry.isIntersecting) {
                                     UrlUpdate.disconnect();
-                                    window.parent.postMessage([ Content.title, URL ], self.Origin);
+                                    self.Log("無盡翻頁", self.NextPage);
+                                    window.parent.postMessage([Content.title, URL], self.Origin);
                                 }
                             });
                         }, {
                             threshold: .1
                         });
-                        UrlUpdate.observe(self.$$("#mangalist img", {
-                            root: Content
-                        }));
+                        UrlUpdate.observe(TopImg);
+                        if (Optimized) {
+                            self.$$("title").id = self.Id.Title;
+                            const ReleaseMemory = new IntersectionObserver(observed => {
+                                observed.forEach(entry => {
+                                    if (entry.isIntersecting) {
+                                        ReleaseMemory.disconnect();
+                                        self.Get_Nodes(document).forEach(node => {
+                                            node.remove();
+                                        });
+                                        TopImg.scrollIntoView();
+                                    }
+                                });
+                            }, {
+                                threshold: .5
+                            });
+                            ReleaseMemory.observe(TopImg);
+                        }
                     };
                     iframe.onerror = () => {
                         iframe.src = self.NextPage;
@@ -432,9 +485,9 @@
                 }
             }
         }
-        async SettingMenu() {}
+        async SettingMenu() { }
         async MenuStyle() {
-            this.AddStyle(``, "Inject_MenuStyle");
+            this.AddStyle(``, this.Id.Menu);
         }
         async Injection() {
             this.BlockAds();
